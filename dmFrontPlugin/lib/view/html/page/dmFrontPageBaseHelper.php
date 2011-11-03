e062421d80ef5bef202471b5768a4ac236ce26ba
Lionel Fenneteau <lionel.fenneteau@gmail.com>
2011-11-03 12:43:07 +0100

    Menage des dumps Ajout widget vide invisible

--- dmFrontPlugin/lib/view/html/page/dmFrontPageBaseHelper.php
+++ dmFrontPlugin/lib/view/html/page/dmFrontPageBaseHelper.php
@@ -363,7 +363,12 @@ abstract class dmFrontPageBaseHelper extends dmConfigurable
 		 */
 		$html .= '</div>';
 
-		return $html;
+    		// si le widget-inner est vide alors on n'affiche pas le widget
+    		if (strip_tags($this->renderWidgetInner($widget) == '')){
+       			return ''; 
+   		} else {
+        		return $html;
+    		}
 	}
 
 	protected function executeWidgetAction(array $widget)
