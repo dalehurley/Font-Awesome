b0961bf57d2bfe860164c652fa7a0fbc6a30eee8
stephane meron <smeron@lesechos.fr>
2011-11-03 15:04:09 +0100

    Remise à jour du handWidgetIntroPageCabinet. Un bloc contenant un extrait d'une page du cabinet choisie dans une liste déroulante. On peut choisir de changer le titre du bloc

--- dmCorePlugin/plugins/sidWidgetCabinetPlugin/modules/handWidgets/templates/_introPageCabinet.php
+++ dmCorePlugin/plugins/sidWidgetCabinetPlugin/modules/handWidgets/templates/_introPageCabinet.php
@@ -1,18 +1,14 @@
 <?php
-
-// var $pageCabinet - $lenght
 if (count($pageCabinet)) { // si nous avons des actu articles
-    echo _tag('h4.title', $pageCabinet[0]->getTitleEntetePage());
+    echo _tag('h4.title', $titlePage);
     echo _open('ul.elements');
-    //lien vers l'image
-    include_partial("objectPartials/introCabinet", array("pageCabinet" => $pageCabinet, "lenght" => $lenght));
+        include_partial("objectPartials/introCabinet", array("pageCabinet" => $pageCabinet, "lenght" => $lenght));
     echo _close('ul.elements');
-
     echo _open('div.navigation.navigationBottom');
-    echo _open('ul.elements');
-    echo _open('li.element');
-    echo _link($pageCabinet[0])->text(__('Learn more about').' {{nomcabinet}}');
-    echo _close('li');
-    echo _close('ul');
+        echo _open('ul.elements');
+            echo _open('li.element');
+                echo _link($pageCabinet)->text(__('Learn more about').' {{nomcabinet}}');
+            echo _close('li');
+        echo _close('ul');
     echo _close('div');
 } // sinon on affiche rien
