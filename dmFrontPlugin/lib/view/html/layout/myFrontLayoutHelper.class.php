c43ba6b5edc7926d6c0e79f63779209547218280
Arnaud Gaudin <arnaudgaudin86@gmail.com>
2011-11-10 11:51:52 +0100

    Modification LayoutHelper
    Intégrations des derniers snippets pour le head

--- dmFrontPlugin/lib/view/html/layout/myFrontLayoutHelper.class.php
+++ dmFrontPlugin/lib/view/html/layout/myFrontLayoutHelper.class.php
@@ -18,6 +18,7 @@ class myFrontLayoutHelper extends dmFrontLayoutHelper {
 		$this->renderHttpMetas().
 		$this->renderMetas().
 		$this->renderFavicon().
+		$this->renderTouchIcon().
 		$this->renderStylesheets().
 		$this->renderHeadJavascripts().
 		$this->renderIeHtml5Fix();
@@ -30,9 +31,9 @@ class myFrontLayoutHelper extends dmFrontLayoutHelper {
 
 		if ($this->isHtml5() || $this->isHtml4)
 		{
-			$htmlTag = sprintf('<!--[if lt IE 7]> <html class="no-js ie6" lang="%s"> <![endif]-->', $culture)."\n".
-					   sprintf('<!--[if IE 7]> <html class="no-js ie7" lang="%s"> <![endif]-->', $culture)."\n".
-					   sprintf('<!--[if IE 8]> <html class="no-js ie8" lang="%s"> <![endif]-->', $culture)."\n".
+			$htmlTag = sprintf('<!--[if lt IE 7]> <html class="no-js ie6 oldie" lang="%s"> <![endif]-->', $culture)."\n".
+					   sprintf('<!--[if IE 7]> <html class="no-js ie7 oldie" lang="%s"> <![endif]-->', $culture)."\n".
+					   sprintf('<!--[if IE 8]> <html class="no-js ie8 oldie" lang="%s"> <![endif]-->', $culture)."\n".
 					   sprintf('<!--[if gt IE 8]><!--> <html class="no-js" lang="%s"> <!--<![endif]-->', $culture);
 		}
 		else
@@ -165,13 +166,13 @@ class myFrontLayoutHelper extends dmFrontLayoutHelper {
 		return $html;
 	}
 
-	//Remplacement de html5shiv par Modernizr pour l'intÃ©gration html5
+	//Remplacement de html5shiv par Modernizr pour l'intégration html5
 	/*
 	 * Ancien lien : //ajax.cdnjs.com/ajax/libs/modernizr/1.7/modernizr-1.7.min.js
 	 *
 	 * Configuration de la version 2 de Modernizr
 	 * CSS3 : tout activer sauf : Flexible Box Model (flexbox), CSS Generated Content
-	 * HTML5 : activer les paramÃ¨tres suivant : Canvas, Canvas Text, HTML5 Audio, HTML5 Video, Input Attributes, Input Types
+	 * HTML5 : activer les paramètres suivant : Canvas, Canvas Text, HTML5 Audio, HTML5 Video, Input Attributes, Input Types
 	 * Extra : HTML5 Shim/IEPP, Modernizr.load, MQ Polyfill, Media Queries, Add CSS Classes, Modernizr.testStyles(), Modernizr.testProp(), Modernizr.testAllProps(), Modernizr._prefixes, Modernizr._domPrefixes
 	 */
 	public function renderIeHtml5Fix()
@@ -191,25 +192,62 @@ class myFrontLayoutHelper extends dmFrontLayoutHelper {
 		return $html;
 	}
 
-	//Rajout fonction personnalisÃ©e pour avoir l'icone Apple
-	protected function getAppleTouchicon()
+	//Rajout fonction personnalisée pour avoir l'icone Apple
+	protected function getTouchIcon($size)
 	{
 		foreach(array('ico', 'png', 'gif') as $extension)
 		{
-			if (file_exists(sfConfig::get('sf_web_dir').'/apple-touch-icon.'.$extension))
-			{
-				return 'apple-touch-icon.'.$extension;
+			//composition du nom du fichier
+			if($size){
+				$appleIcon = sfConfig::get('sf_web_dir').'/apple-touch-icon-'.$size.'-precomposed.'.$extension;
+			}else{
+				$appleIcon = sfConfig::get('sf_web_dir').'/apple-touch-icon-precomposed.'.$extension;
+			}
+			
+			if (file_exists($appleIcon)) {
+				return $appleIcon;
 			}
 		}
 	}
+	
+	//Gestion des icônes pour les appareils mobiles
+	public function renderTouchIcon() {
+		$touchIcon = $this->getTouchIcon();
+		$touchIcon72 = $this->getTouchIcon('72x72');
+		$touchIcon114 = $this->getTouchIcon('114x114');
+		
+		//création html
+		$html = '';
+		
+		//rajout des icones précomposées à différentes résolutions
+		if ($touchIcon) {
+			$html.= sprintf('<link rel="apple-touch-icon" href="%s/%s" />',
+						dmArray::get($this->serviceContainer->getParameter('request.context'), 'relative_url_root'),
+						$touchIcon
+						)."\n";
+		}
+		if ($touchIcon72) {
+			$html.= sprintf('<link rel="apple-touch-icon" sizes="72x72" href="%s/%s" />',
+						dmArray::get($this->serviceContainer->getParameter('request.context'), 'relative_url_root'),
+						$touchIcon72
+						)."\n";
+		}
+		if ($touchIcon114) {
+			$html.= sprintf('<link rel="apple-touch-icon" sizes="114x114" href="%s/%s" />',
+						dmArray::get($this->serviceContainer->getParameter('request.context'), 'relative_url_root'),
+						$touchIcon114
+						)."\n";
+		}
+		
+		return $html;
+	}
 
-	//Modification pour gestion des Apple Touch Icon, simplification html5 (suppression de 'type="image/x-icon"')
+	//Simplification html5 (suppression de 'type="image/x-icon"')
 	public function renderFavicon()
 	{
 		$favicon = $this->getFavicon();
-		$appletouchIcon = $this->getAppleTouchicon();
 
-		//crÃ©ation html
+		//création html
 		$html = '';
 
 		if ($favicon)
@@ -219,13 +257,7 @@ class myFrontLayoutHelper extends dmFrontLayoutHelper {
 						$favicon
 						)."\n";
 		}
-		if ($appletouchIcon)
-		{
-			$html.= sprintf('<link rel="apple-touch-icon" href="%s/%s" />',
-						dmArray::get($this->serviceContainer->getParameter('request.context'), 'relative_url_root'),
-						$appletouchIcon
-						)."\n";
-		}
+		
 		return $html;
 	}
 
@@ -240,8 +272,8 @@ class myFrontLayoutHelper extends dmFrontLayoutHelper {
 			'language'		=> $this->serviceContainer->getParameter('user.culture')
 	    );
 		
+		//à modifier une fois la mise en prod pour autoriser le zoom (désactiver maximum-scale=1.0, user-scalable=0)
 		$metas['viewport'] = 'width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=0';
-		//width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=0
 
 		if (sfConfig::get('dm_seo_use_keywords') && $keywords = $this->page->get('keywords'))
 		{
@@ -261,11 +293,11 @@ class myFrontLayoutHelper extends dmFrontLayoutHelper {
 		return $metas;
 	}
 
-	//Optimisation intÃ©gration code Google Analytics, Ã  mettre Ã  jour en fonction des derniÃ¨res optimisations :
+	//Optimisation intégration code Google Analytics, à  mettre à jour en fonction des dernières optimisations :
 	//mathiasbynens.be/notes/async-analytics-snippet
 	protected function getGoogleAnalyticsCode($gaKey)
 	{
-		//utilisation de la version optimisÃ©e de 293 octets
+		//utilisation de la version optimisée de 293 octets
 		$html = "<script>
 					var _gaq = [['_setAccount', '".$gaKey."'], ['_trackPageview']];
 					(function(d, t) {
