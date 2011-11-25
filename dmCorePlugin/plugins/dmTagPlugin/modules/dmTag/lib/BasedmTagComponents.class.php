451ed3f26101cdb8439b22b7326549e42a471b7a
Arnaud Gaudin <arnaudgaudin86@gmail.com>
2011-11-25 15:46:53 +0100

    Compilation séparée de DmTagPopular et ajout appel CSS dans le module

--- dmCorePlugin/plugins/dmTagPlugin/modules/dmTag/lib/BasedmTagComponents.class.php
+++ dmCorePlugin/plugins/dmTagPlugin/modules/dmTag/lib/BasedmTagComponents.class.php
@@ -13,6 +13,9 @@ class BasedmTagComponents extends myFrontModuleComponents
   public function executePopular()
   {
     $this->dmTags = dmDb::table('DmTag')->getPopularTags(array(), 100);
+	
+	//insertion de la CSS du widget du theme courant
+	$this->getResponse()->addStylesheet(sfConfig::get('sf_css_path_template').'/Widgets/DmTagPopular/DmTagPopular.css');
   }
 
   public function executeShow()
