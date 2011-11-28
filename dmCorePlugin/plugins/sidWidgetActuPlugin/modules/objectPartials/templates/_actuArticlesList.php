5e720903f32adcd8211adea6aa38f552618f8c7f
stephane meron <smeron@lesechos.fr>
2011-11-28 13:35:32 +0100

    Evolution du plugin sidWidgetActuPlugin

--- dmCorePlugin/plugins/sidWidgetActuPlugin/modules/objectPartials/templates/_actuArticlesList.php
+++ dmCorePlugin/plugins/sidWidgetActuPlugin/modules/objectPartials/templates/_actuArticlesList.php
@@ -6,7 +6,7 @@
 $i = 1;
     
     // premiÃ¨re boucle des 2 premiers articles avec photo, titre et chapeau (rÃ©sumÃ©)
-    if ($i <= 2) {
+//    if ($i <= 2) {
         $html = '';
         
         if(($article->image) != NULL){
@@ -16,34 +16,29 @@ $i = 1;
         };
         $html .= _open('span.wrapper');
         $html .= _tag('span.title',$article);
-        $html .= _tag('span.title','Sujet : '.$article);
         $html .= _tag('span.teaser',$article->getResume());
         $html .= _close('span.wrapper');
         echo _open('li.element');
         echo _link($article)->text($html)->set('.link_box');
         echo _close('li');
-        }
-        else{
-            echo _open('li.element');
-           echo _link($article)->text(_tag('span.title',$article).' '._tag('span.teaser',$article->getResume()))->set('.link_box');
-        }
-        echo _close('li');
+//        }
+//        else{
+//            echo _open('li.element');
+//           echo _link($article)->text(_tag('span.title',$article).' '._tag('span.teaser',$article->getResume()))->set('.link_box');
+//        }
+//        echo _close('li');
     
     // Ã  partir du 3Ã¨me, prÃ©sentation en lien sur le titre
-    if ($i >= 6) {
-        
-
-        echo _open('li.element');
-
-        echo _link($article)->text(ucfirst($article->getRubrique()).' > '.$article)->set('.link_box');
-
-        echo _close('li');
-    }
+//    if ($i >= 6) {
+//        
+//
+//        echo _open('li.element');
+//
+//        echo _link($article)->text(ucfirst($article->getRubrique()).' > '.$article)->set('.link_box');
+//
+//        echo _close('li');
+//    }
 
     
      
-    $i++;
-    
-
-//
-echo _close('ul');
+//    $i++;
