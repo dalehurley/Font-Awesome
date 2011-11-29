48b7896e6b77718c21afbfeaf7243aedc0547824
lionel Fenneteau <lionel.fenneteau@gmail.com>
2011-11-29 11:02:46 +0100

    Correction bug sur dmWidgetNavigationMenuView venant de myWidgetNavigationMenuView qui initialise la ulClass avant

--- dmFrontPlugin/lib/dmWidget/menu/dmWidgetNavigationMenuView.php
+++ dmFrontPlugin/lib/dmWidget/menu/dmWidgetNavigationMenuView.php
@@ -1,72 +1,67 @@
 <?php
 
-class dmWidgetNavigationMenuView extends dmWidgetPluginView
-{
-  protected
-  $isIndexable = false;
+class dmWidgetNavigationMenuView extends dmWidgetPluginView {
 
-  public function configure()
-  {
-    parent::configure();
+    protected
+    $isIndexable = false;
 
-    $this->addRequiredVar(array('items', 'ulClass', 'liClass'));
-  }
+    public function configure() {
+        parent::configure();
 
-  protected function filterViewVars(array $vars = array())
-  {
-    $vars = parent::filterViewVars($vars);
-
-    $menuClass = dmArray::get($vars, 'menuClass');
-    $menuName = dmArray::get($vars, 'menuName');
-
-    $vars['menu'] = $this->getService('menu', $menuClass ? $menuClass : null)
-    ->ulClass($vars['ulClass']);
-    if (!is_null($menuName)) {
-	    $vars['menu']->name($menuName);
+        $this->addRequiredVar(array('items', 'ulClass', 'liClass'));
     }
 
-    foreach($vars['items'] as $index => $item)
-    {
-      $menuItem = $vars['menu']
-      ->addChild($index.'-'.dmString::slugify($item['text']), $item['link'])
-      ->label($item['text'])
-      ->secure(!empty($item['secure']))
-      ->liClass($vars['liClass'])
-      ->addRecursiveChildren(dmArray::get($item, 'depth', 0));
+    protected function filterViewVars(array $vars = array()) {
+        $vars = parent::filterViewVars($vars);
 
-      if(!empty($item['nofollow']) && $menuItem->getLink())
-      {
-        $menuItem->getLink()->set('rel', 'nofollow');
-      }
+        $menuClass = dmArray::get($vars, 'menuClass');
+        $menuName = dmArray::get($vars, 'menuName');
 
-      if(!empty($item['target']) && $menuItem->getLink())
-      {
-        $menuItem->getLink()->target($item['target']);
-      }
+        $vars['menu'] = $this->getService('menu', $menuClass ? $menuClass : null);
+        
+        if (isset($vars['ulClass'])) { // correction suite ajout de myWidgetNavigationMenuVien qui redéfini le ulClass
+            $vars['menu']->ulClass($vars['ulClass']);
+        }
+        if (!is_null($menuName)) {
+            $vars['menu']->name($menuName);
+        }
+
+        foreach ($vars['items'] as $index => $item) {
+            $menuItem = $vars['menu']
+                    ->addChild($index . '-' . dmString::slugify($item['text']), $item['link'])
+                    ->label($item['text'])
+                    ->secure(!empty($item['secure']))
+                    ->liClass($vars['liClass'])
+                    ->addRecursiveChildren(dmArray::get($item, 'depth', 0));
+
+            if (!empty($item['nofollow']) && $menuItem->getLink()) {
+                $menuItem->getLink()->set('rel', 'nofollow');
+            }
+
+            if (!empty($item['target']) && $menuItem->getLink()) {
+                $menuItem->getLink()->target($item['target']);
+            }
+        }
+
+        unset($vars['items'], $vars['ulClass'], $vars['liClass']);
+
+        return $vars;
     }
 
-    unset($vars['items'], $vars['ulClass'], $vars['liClass']);
+    protected function doRender() {
+        if ($this->isCachable() && $cache = $this->getCache()) {
+            return $cache;
+        }
 
-    return $vars;
-  }
+        $vars = $this->getViewVars();
 
-  protected function doRender()
-  {
-    if ($this->isCachable() && $cache = $this->getCache())
-    {
-      return $cache;
+        $html = $vars['menu']->render();
+
+        if ($this->isCachable()) {
+            $this->setCache($html);
+        }
+
+        return $html;
     }
-
-    $vars = $this->getViewVars();
-
-    $html = $vars['menu']->render();
-
-    if ($this->isCachable())
-    {
-      $this->setCache($html);
-    }
-
-    return $html;
-  }
 
 }
