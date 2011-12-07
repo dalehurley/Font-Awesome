6b972e199450d931d38d88b4a17b2e505a56bd5e
stephane meron <smeron@lesechos.fr>
2011-12-07 11:19:55 +0100

    Purge des HandWidgets du plugin sidWidgetUtilesPlugin

--- dmCorePlugin/plugins/sidWidgetSitesUtilesPlugin/modules/indexSitesUtiles/templates/_show.php
+++ dmCorePlugin/plugins/sidWidgetSitesUtilesPlugin/modules/indexSitesUtiles/templates/_show.php
@@ -1,4 +1,8 @@
 <?php // Vars: $indexSitesUtiles
 
-echo _tag('h2',$indexSitesUtiles);
+echo _tag('h2.title',$indexSitesUtiles);
+echo _open('span.wrapper');
+echo _open('span.teaser');
 echo $indexSitesUtiles->description;
+echo _close('span.teaser');
+echo _close('span.wrapper');
