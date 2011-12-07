7f117ba7396de69f309e789264204e72b93c5af7
stephane meron <smeron@lesechos.fr>
2011-12-07 11:15:13 +0100

    Purge des HandWidgets du plugin sidWidgetCabinetPlugin

--- dmCorePlugin/plugins/sidWidgetCabinetPlugin/modules/handWidgets/templates/_missionShow.php
+++
@@ -1,5 +0,0 @@
-<?php
-if (count($missions)) { // si nous avons des actu articles
-    echo _tag('h2.title', $titreBloc);
-        include_partial("objectPartials/missionShow", array("mission" => $missions));
-} // sinon on affiche rien
