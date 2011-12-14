0323bc6e2b4349861769840dbca24ee972c07628
Arnaud Gaudin <arnaudgaudin86@gmail.com>
2011-12-14 16:37:29 +0100

    Ménage dans les anciens code du widget de debug

--- dmCorePlugin/plugins/sidSPLessCssPlugin/modules/sidSPLessCss/actions/components.class.php
+++ dmCorePlugin/plugins/sidSPLessCssPlugin/modules/sidSPLessCss/actions/components.class.php
@@ -10,11 +10,8 @@ class sidSPLessCssComponents extends myFrontModuleComponents {
 		//insertion de la CSS du widget du theme courant
 		$this->getResponse()->addStylesheet(sfConfig::get('sf_css_path_template').'/Widgets/SidSPLessCssDebug/SidSPLessCssDebug.css');
 		
-        //Gabarit de la page visible en environnement de dev
-        $currentGabarit = sfContext::getInstance()->getPage()->get('gabarit');
-        if ($currentGabarit == 'default') {
-            $currentGabarit = spLessCss::getLessParam('templateGabarit');
-        }
+		//récupération des valeurs de configuration par défaut de la page
+		$pageTemplateOptionsDefault = spLessCss::pageTemplateGetOptionsDefault();
 		
 		//stockage des paramètres à afficher
 		$paramSpLessCss = array(
@@ -48,7 +45,7 @@ class sidSPLessCssComponents extends myFrontModuleComponents {
 							),
 							array(
 								'info'	=>	'currentGabarit',
-								'value' =>	$currentGabarit
+								'value' =>	$pageTemplateOptionsDefault['currentGabarit']
 							),
 							array(
 								'info'	=>	'gridContainer',
@@ -68,6 +65,7 @@ class sidSPLessCssComponents extends myFrontModuleComponents {
 						);
 		
 		
+		//déclaration des variables se remplissant avec les valeurs et propriétés à afficher
 		$debugParam = array();
 		$debugDisplay = '';
 		
@@ -87,39 +85,7 @@ class sidSPLessCssComponents extends myFrontModuleComponents {
 		
 		$this->html.= $debugDisplay;
 		
-		/*,
-							_tag('span.info.mainTemplate', 'mainTemplate : ' . _tag('span.value', spLessCss::getLessParam('mainTemplate'))) . tag('br') .
-							_tag('span.info.version', 'templateVersion : ' . _tag('span.value', spLessCss::getLessParam('templateVersion'))) . tag('br') .
-							_tag('span.info.date', 'templateDate : ' . _tag('span.value', spLessCss::getLessParam('templateDate'))) . tag('br') .
-							_tag('span.info.gabarit', 'templateGabarit : ' . _tag('span.value', spLessCss::getLessParam('templateGabarit'))) . tag('br') .
-							_tag('span.info.grid', 'templateGrid : ' . _tag('span.value', spLessCss::getLessParam('templateGrid'))) . tag('br') .
-							_tag('span.info.copyright', 'templateCopyright : ' . _tag('span.value', spLessCss::getLessParam('templateCopyright'))) . tag('br') .
-							_tag('span.info.author', 'templateAuthor : ' . _tag('span.value', spLessCss::getLessParam('templateAuthor'))) . tag('br') .
-							_tag('span.info.currentGabarit', 'currentGabarit : ' . _tag('span.value', $currentGabarit)) . tag('br') .
-							_tag('span.info.gridContainer', 'gridContainer : ' . _tag('span.value', spLessCss::getLessParam('gridContainer'))) . tag('br') .
-							_tag('span.info.gridColWidth', 'gridColWidth : ' . _tag('span.value', spLessCss::getLessParam('gridColWidth'))) . tag('br') .
-							_tag('span.info.gridGutter', 'gridGutter : ' . _tag('span.value', spLessCss::getLessParam('gridGutter'))) . tag('br') .
-							_tag('span.info.screenType', 'screenType : ' . _tag('span.value')) . tag('br')
-							);*/
-		
 		$this->html.= _close('div.debugInfo');
 		$this->html.= _close('div.debugTemplate');
-		/*
-		$this->html = _tag('div.debugTemplate',
-						_tag('div.debugInfo',
-							_tag('span.info.mainTemplate', 'mainTemplate : ' . _tag('span.value', spLessCss::getLessParam('mainTemplate'))) . tag('br') .
-							_tag('span.info.version', 'templateVersion : ' . _tag('span.value', spLessCss::getLessParam('templateVersion'))) . tag('br') .
-							_tag('span.info.date', 'templateDate : ' . _tag('span.value', spLessCss::getLessParam('templateDate'))) . tag('br') .
-							_tag('span.info.gabarit', 'templateGabarit : ' . _tag('span.value', spLessCss::getLessParam('templateGabarit'))) . tag('br') .
-							_tag('span.info.grid', 'templateGrid : ' . _tag('span.value', spLessCss::getLessParam('templateGrid'))) . tag('br') .
-							_tag('span.info.copyright', 'templateCopyright : ' . _tag('span.value', spLessCss::getLessParam('templateCopyright'))) . tag('br') .
-							_tag('span.info.author', 'templateAuthor : ' . _tag('span.value', spLessCss::getLessParam('templateAuthor'))) . tag('br') .
-							_tag('span.info.currentGabarit', 'currentGabarit : ' . _tag('span.value', $currentGabarit)) . tag('br') .
-							_tag('span.info.gridContainer', 'gridContainer : ' . _tag('span.value', spLessCss::getLessParam('gridContainer'))) . tag('br') .
-							_tag('span.info.gridColWidth', 'gridColWidth : ' . _tag('span.value', spLessCss::getLessParam('gridColWidth'))) . tag('br') .
-							_tag('span.info.gridGutter', 'gridGutter : ' . _tag('span.value', spLessCss::getLessParam('gridGutter'))) . tag('br') .
-							_tag('span.info.screenType', 'screenType : ' . _tag('span.value')) . tag('br')
-							)
-						);*/
 	}
 }
