ec5a3c02e494ec903716eececbff04909b188507
stephane meron <smeron@lesechos.fr>
2011-12-28 16:56:14 +0100

    nettoyage des fichiers de sidWidgetCabinetPlugin

---
+++ dmCorePlugin/plugins/sidWidgetCabinetPlugin/modules/recrutement/actions/components.class.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Recrutement components
+ * 
+ * No redirection nor database manipulation ( insert, update, delete ) here
+ */
+class recrutementComponents extends myFrontModuleComponents
+{
+
+
+}
