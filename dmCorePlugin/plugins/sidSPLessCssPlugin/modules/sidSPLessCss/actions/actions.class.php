9898915947fc655ce24b922dffde8facc7a50522
Arnaud Gaudin <arnaudgaudin86@gmail.com>
2012-01-03 12:12:37 +0100

    Déclaration de l'action dans le plugin pour la génération des sprites en AJAX

--- dmCorePlugin/plugins/sidSPLessCssPlugin/modules/sidSPLessCss/actions/actions.class.php
+++ dmCorePlugin/plugins/sidSPLessCssPlugin/modules/sidSPLessCss/actions/actions.class.php
@@ -4,6 +4,21 @@
  */
 class sidSPLessCssActions extends myFrontModuleActions
 {
-
-
+	public function executeSpriteInit(sfWebRequest $request) {
+		
+		//on vérifie que l'appel est bien fait en AJAX
+		if($request->isXmlHttpRequest()) {
+			
+			//on récupère les valeurs passées dans la requête
+			$parameters = $request->getGetParameters();
+			
+			//lancement de l'initialisation des sprites et récupération des valeurs d'avancement
+			$spriteValues = json_encode(spLessCss::spriteInit($spriteFormat = "S"));
+			
+			//$testJson = json_encode(array('prct' => 50, 'testResult' => 'Message de fin de test JSON'));
+			
+			return $this->renderText($spriteValues);
+			//return $this->renderText('Génération AJAX terminée');
+		}
+	}
 }
