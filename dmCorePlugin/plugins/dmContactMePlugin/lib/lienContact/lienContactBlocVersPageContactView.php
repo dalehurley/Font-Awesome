4b78eaa349a4851b78bd72aa85186247abfdbc06
stephane meron <smeron@lesechos.fr>
2012-01-03 11:52:35 +0100

    rajout du bloc pour lien vers page de contact

---
+++ dmCorePlugin/plugins/dmContactMePlugin/lib/lienContact/lienContactBlocVersPageContactView.php
@@ -0,0 +1,33 @@
+<?php
+
+class lienContactBlocVersPageContactView extends dmWidgetPluginView {
+
+    public function configure() {
+        parent::configure();
+
+        $this->addRequiredVar(array(
+            'titreBloc',
+            'titreLien',
+            'message'
+        ));
+    }
+
+    /**
+     * On affiche NB articles Actu selon 3 types:
+     * 1) il est sur une dmPage automatique de type "Rubrique" : on affiche les articles qui sont dans les sections de cette rubrique
+     * 2) il est sur une dmPage automatique de type "Section" : on affiche les articles qui sont dans cette section
+     * 3) il est sur une page ni Rubrique ni Section, automatique ou pas : on affiche les articles de la rubrique choisie par défaut
+     *  
+     */
+    protected function doRender() {
+        
+         $vars = $this->getViewVars();
+        return $this->getHelper()->renderPartial('lienContact', 'blocVersPageContact', array(
+                    'titreBloc' => $vars['titreBloc'],
+                    'titreLien' => $vars['titreLien'],
+                    'message' => $vars['message'],
+            
+                ));        
+
+}
+}
