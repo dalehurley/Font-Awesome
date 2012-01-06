eb5d4cc430977893f478f5825aa68fdc9aba5971
stephane meron <smeron@lesechos.fr>
2012-01-06 15:16:03 +0100

    modif de dmMenu pour trier les sous menus

--- dmCorePlugin/lib/menu/dmMenu.php
+++ dmCorePlugin/lib/menu/dmMenu.php
@@ -442,10 +442,10 @@ class dmMenu extends dmConfigurable implements ArrayAccess, Countable, IteratorA
       dmDb::table('DmPage')->createQuery('p')
       ->withI18n($this->user->getCulture(), null, 'p')
       ->select('p.*, pTranslation.*')
-// ajout stef le 04/01/2012 pour classer les fils par ordre alphabétique
-      ->orderBy('pTranslation.title ASC')
+// ajout stef pour trier par ordre d'integration les sous menus (utiles pour les sections des rubriques dans le menu de gauche)
+            ->orderBy('p.record_id')
+// fin ajout
     );
-
     if ($pageChildren = $this->getLink()->getPage()->getNode()->getChildren()) {
             foreach ($pageChildren as $childPage) {
                 // ajout lionel
