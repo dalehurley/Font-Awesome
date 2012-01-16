8b9df8855630cb513c268a837acf65283efbb66d
Arnaud Gaudin <arnaudgaudin86@gmail.com>
2012-01-16 16:24:34 +0100

    Meilleur gestion des liens dans le partial de listing

--- dmFrontPlugin/templates/_publicationListElement.php
+++ dmFrontPlugin/templates/_publicationListElement.php
@@ -1,7 +1,7 @@
 <?php
 /*
  * _publicationListElement.php
- * v0.2
+ * v0.3
  * Permet d'afficher une publication de façon simplifié à l'intérieur d'une liste ul li
  * 
  * Variables disponibles :
@@ -51,68 +51,77 @@ switch ($itemType) {
 
 //ouverture container de publication
 $html.= _open('li.element' . $posClass, $ctnOpts);
+	
+	//variable de remplissage du li
+	$htmlLi = '';
+	
+	//création de l'image seulement si présente
+	if($isImage){
+		//calcul des dimensions de l'image
+		$imgColWidth = ($itemType == 'Person') ? spLessCss::getLessParam('thumbM_col') : spLessCss::getLessParam('thumbL_col');
+		$imgColHeight = ($itemType == 'Person') ? spLessCss::getLessParam('thumbM_bl') * 2 : spLessCss::getLessParam('thumbL_bl');
 
-	//ouverture du lien si nécessaire
-	if(isset($linkUrl)) $html.= _open('a.link.link_box', array('href' => $linkUrl, 'title' => $title));
+		$htmlLi.= get_partial('global/imageWrapper', array(
+													'image'	=>	$image,
+													'alt'	=>	$title,
+													'width'	=>	spLessCss::gridGetWidth($imgColWidth,0),
+													'height'=>	spLessCss::gridGetHeight($imgColHeight,0)
+													));
+	}
 
-		//création de l'image seulement si présente
-		if($isImage){
-			//calcul des dimensions de l'image
-			$imgColWidth = ($itemType == 'Person') ? spLessCss::getLessParam('thumbM_col') : spLessCss::getLessParam('thumbL_col');
-			$imgColHeight = ($itemType == 'Person') ? spLessCss::getLessParam('thumbM_bl') * 2 : spLessCss::getLessParam('thumbL_bl');
+	//si une image est présente on englobe le texte dans un wrapper pour faciliter son placement CSS par rapport à cette dernière
+	if($isImage) $htmlLi.= _open('span.wrapper');
 
-			$html.= get_partial('global/imageWrapper', array(
-														'image'	=>	$image,
-														'alt'	=>	$title,
-														'width'	=>	spLessCss::gridGetWidth($imgColWidth,0),
-														'height'=>	spLessCss::gridGetHeight($imgColHeight,0)
-														));
-		}
+		//créer des partials pour les différents sous-éléments
+		if($itemType == 'Person'){
+			$htmlLi.= _open('div.subWrapper');
+				$htmlLi.= _tag('span.name itemprop="name"', $node->getTitle());
+				$htmlLi.= '&#160;-&#160;';
+				$htmlLi.= _tag('span.jobTitle itemprop="jobTitle"', $node->getStatut());
+			$htmlLi.= _close('div');
 
-		//si une image est présente on englobe le texte dans un wrapper pour faciliter son placement CSS par rapport à cette dernière
-		if($isImage) $html.= _open('span.wrapper');
-			
-			//créer des partials pour les différents sous-éléments
-			if($itemType == 'Person'){
-				$html.= _open('div.subWrapper');
-					$html.= _tag('span.name itemprop="name"', $node->getTitle());
-					$html.= '&#160;-&#160;';
-					$html.= _tag('span.jobTitle itemprop="jobTitle"', $node->getStatut());
-				$html.= _close('div');
-				
-				$html.= _open('span.telephone');
-					$html.= _tag('span.type', __('phone'));
-					$html.= '&#160;';
-					$html.= _tag('span.value itemprop="telephone"', $node->getTel());
-				$html.= _close('span');
-				
-				if(isset($rubrique)) {
-					if($rubrique != null) {
-						$html.= _open('span.rubrique');
-							$html.= _tag('span.type', __('Responsable in'));
-							$html.= '&nbsp;:&nbsp;';
-							$html.= _tag('span.value', $rubrique);
-						$html.= _close('span');
-					}
+			$htmlLi.= _open('span.telephone');
+				$htmlLi.= _tag('span.type', __('phone'));
+				$htmlLi.= '&#160;';
+				$htmlLi.= _tag('span.value itemprop="telephone"', $node->getTel());
+			$htmlLi.= _close('span');
+
+			if(isset($rubrique)) {
+				if($rubrique != null) {
+					$htmlLi.= _open('span.rubrique');
+						$htmlLi.= _tag('span.type', __('Responsable in'));
+						$htmlLi.= '&nbsp;:&nbsp;';
+						$htmlLi.= _tag('span.value', $rubrique);
+					$htmlLi.= _close('span');
+				}
+			}
+
+			$htmlLi.= _open('span.email');
+				$htmlLi.=  _tag('span.type', __('Email'));
+				$htmlLi.=  '&nbsp;:&nbsp;';		
+				if(isset($linkUrl)){
+					$htmlLi.= _tag('span.value itemprop="email"', $node->getEmail());
+				}else{
+					$htmlLi.= _tag('span.value', _link('mailto:' . $node->getEmail())->text($node->getEmail())->set('.link itemprop="email"'));
 				}
 				
-				$html.= _open('span.email');
-					$html.=  _tag('span.type', __('Email'));
-					$html.=  '&nbsp;:&nbsp;';
-					$html.=  _tag('span.value', _link('mailto:' . $node->getEmail())->text($node->getEmail())->set('itemprop="email"'));
-				$html.=  _close('span');
-				
-				$html.= get_partial('global/descriptionWrapper', array('teaser' => $node->getText()));
-			}else{
-				if(isset($title))										$html.= get_partial('global/titleWrapper', array('title' => $title));
-				if(isset($node->created_at) && $itemType == 'Article')	$html.= get_partial('global/dateWrapperShort', array('node' => $node));
-				if(isset($teaser))										$html.= get_partial('global/teaserWrapper', array('teaser' => $teaser));
-			}
-			
-		if($isImage) $html.= _close('span.wrapper');
+			$htmlLi.=  _close('span');
 
-	//fermeture du lien si nécessaire
-	if(isset($linkUrl)) $html.= _close('a.link.link_box');
+			$htmlLi.= get_partial('global/descriptionWrapper', array('teaser' => $node->getText()));
+		}else{
+			if(isset($title))										$htmlLi.= get_partial('global/titleWrapper', array('title' => $title));
+			if(isset($node->created_at) && $itemType == 'Article')	$htmlLi.= get_partial('global/dateWrapperShort', array('node' => $node));
+			if(isset($teaser))										$htmlLi.= get_partial('global/teaserWrapper', array('teaser' => $teaser));
+		}
+
+	if($isImage) $htmlLi.= _close('span.wrapper');
+	
+	//inclusion dans le lien si nécessaire
+	if(isset($linkUrl)) {
+		$html.= _link($linkUrl)->text($htmlLi)->title($title)->set(isset($image) ? '.link_box' : '');
+	}else{
+		$html.= $htmlLi;
+	}
 	
 //fermeture container de publication
 $html.= _close('li.element');
