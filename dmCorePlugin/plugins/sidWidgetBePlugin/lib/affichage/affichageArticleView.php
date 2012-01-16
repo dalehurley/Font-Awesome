52206752d13ae7e9611278c55fe881f78d965383
lionel Fenneteau <lionel.fenneteau@gmail.com>
2012-01-16 12:04:57 +0100

    Maj symfony sub

--- dmCorePlugin/plugins/sidWidgetBePlugin/lib/affichage/affichageArticleView.php
+++ dmCorePlugin/plugins/sidWidgetBePlugin/lib/affichage/affichageArticleView.php
@@ -35,31 +35,31 @@ class affichageArticleView extends dmWidgetPluginView {
      * Si cette fonction n'est pas définie alors l'index de lucene est effectué sur this->doRender(), avec prise en compte du partial... etc... ATTENTION? peut être très long à traiter par Lucene
      * 
      */
-//    protected function doRenderForIndex() {
-//        
-//        $vars = $this->getViewVars();
-//
-//        if ($vars['recordId'] == '') { // donc article contextuel
-//            $record = sfcontext::getInstance()->getPage() ? sfcontext::getInstance()->getPage()->getRecord() : false;
-//            $recordId = $record->id;
-//        } else {
-//            $recordId = $vars['recordId'];
-//        }
-//
-//        $article = dmDb::table('SidArticle') //->findOneBySectionId($section->id);
-//                ->createQuery('a')
-//                ->where('id = ? ', array($recordId))
-//                ->orderBy('updated_at DESC')
-//                ->limit(1)
-//                ->execute();        
-//
-//        $indexRender = '';
-//        if ($article[0]){
-//            $indexRender = stringTools::str_truncate($article[0]->chapeau, 10000); // on indexe seulement le chapeau de l'article, on peut laisse le render() complet mais cela s'avère très lent...
-//        }
-//        
-//        return $indexRender;
-//    }
+    protected function doRenderForIndex() {
+        
+        $vars = $this->getViewVars();
+
+        if ($vars['recordId'] == '') { // donc article contextuel
+            $record = sfcontext::getInstance()->getPage() ? sfcontext::getInstance()->getPage()->getRecord() : false;
+            $recordId = $record->id;
+        } else {
+            $recordId = $vars['recordId'];
+        }
+
+        $article = dmDb::table('SidArticle') //->findOneBySectionId($section->id);
+                ->createQuery('a')
+                ->where('id = ? ', array($recordId))
+                ->orderBy('updated_at DESC')
+                ->limit(1)
+                ->execute();        
+
+        $indexRender = '';
+        if ($article[0]){
+            $indexRender = stringTools::str_truncate($article[0]->chapeau, 200); // on indexe seulement le chapeau de l'article, on peut laisse le render() complet mais cela s'avère très lent...
+        }
+        
+        return $indexRender;
+    }
     
     
 
