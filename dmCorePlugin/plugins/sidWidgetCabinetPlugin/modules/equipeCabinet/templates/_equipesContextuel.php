4802d81504309bdddf5131c46caccdf04d4b77d6
Arnaud Gaudin <arnaudgaudin86@gmail.com>
2012-01-16 17:01:56 +0100

    Modification du widget d'équipe contextuel

--- dmCorePlugin/plugins/sidWidgetCabinetPlugin/modules/equipeCabinet/templates/_equipesContextuel.php
+++ dmCorePlugin/plugins/sidWidgetCabinetPlugin/modules/equipeCabinet/templates/_equipesContextuel.php
@@ -1,21 +1,48 @@
 <?php
 // vars : $equipes, $titreBloc, $titreLien, $pageEquipe, $lenght, $rubrique, $nomRubrique
-if (count($equipes)) { // si nous avons des actu articles
-    if ($titreBloc != '') {
-	echo _tag('h4.title', $titreBloc);
-    }
 
-    echo _open('ul.elements');
+$html = '';
+
+if (count($equipes)) { // si nous avons des actu articles
+	
+	//affichage du titre du bloc
+    if ($titreBloc != '') $html.= _tag('h4.title', $titreBloc);
+	
+	//ouverture du listing
+    $html.= _open('ul.elements');
+	
+	//compteur
+	$count = 0;
+	$maxCount = count($equipes);
+	
     foreach ($equipes as $equipe) {
-	include_partial("objectPartials/introEquipe", array("equipe" => $equipe,"rubrique" => $rubrique, "nomRubrique" => $nomRubrique));
+		//incrémentation compteur
+		$count++;
+		
+		$html.= get_partial('global/publicationListElement', array(
+												'node' => $equipe,
+												'itemType' => 'Person',
+												'title' => $equipe->getTitle(),
+												'image' => $equipe->getImage(),
+												'rubrique' => $nomRubrique[$equipe->id],
+												'count' => $count,
+												'maxCount' => $maxCount,
+												'isLight' => true
+												));
     }
-    echo _close('ul');
+	
+    $html.= _close('ul.elements');
     
+	/*
     echo _open('div.navigation.navigationBottom');
 	echo _open('ul.elements');
 	    echo _open('li.element');
 		echo _link('pageCabinet/equipe')->text($titreLien);
 	    echo _close('li');
 	echo _close('ul');
-    echo _close('div');    
+    echo _close('div');
+	 */
 } // sinon on affiche rien
+
+//affichage html en sortie
+echo $html;
