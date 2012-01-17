8774516dec26a05e5ae99394a272f0a4db0d4c94
Arnaud Gaudin <arnaudgaudin86@gmail.com>
2012-01-17 15:01:53 +0100

    Correction mises en page de widgets

--- dmCorePlugin/plugins/sidWidgetBePlugin/modules/specifiquesBaseEditoriale/templates/_filActualite.php
+++ dmCorePlugin/plugins/sidWidgetBePlugin/modules/specifiquesBaseEditoriale/templates/_filActualite.php
@@ -2,16 +2,37 @@
 //chargement feuille de style spécifique
 use_stylesheet(sfConfig::get('sf_css_path_template'). '/Widgets/SpecifiquesBaseEditorialeFilActualite/SpecifiquesBaseEditorialeFilActualite.css');
 
+$html = '';
+
 // vars : $section, $titreBloc, $titreLien, $longueurTexte, $articles, $arrayRubrique, $photo
 if (count($articles)) { // si nous avons des actu articles
-
-    echo _tag('h4.title', $titreBloc);
-    echo _open('ul.elements');
-    foreach ($articles as $article){
-
-	include_partial("objectPartials/filActualite", array("article" => $article,"textLength" => $longueurTexte,"textEnd" => '(...)','titreBloc' => $titreBloc, "titreLien" => $titreLien,"arrayRubrique" => $arrayRubrique,"photo" => $photo));
-
+	
+	$html.= get_partial('global/titleWidget', array('title' => $titreBloc));
+	
+	//ouverture du listing
+    $html.= _open('ul.elements');
+	
+    foreach ($articles as $article) {
+		//création d'un tableau de liens à afficher
+		$elements = array();
+		$elements[] = array('title' => $titreLien . '&#160;' . $arrayRubrique[$article->filename], 'linkUrl' => $article->Section);
+		
+		$html.= get_partial('global/publicationListElement', array(
+												'node' => $article,
+												'image' => '/_images/lea' . $article->filename . '-p.jpg',
+												'count' => 1,
+												'maxCount' => 1,
+												'teaser' => $article->getChapeau(),
+												'teaserLength' => $longueurTexte,
+												'navigationElements' => $elements,
+												'linkUrl' => $article
+												));
     }
-    echo _close('ul');
+	
+    //fermeture du listing
+    $html.= _close('ul.elements');
     
 }
+
+//affichage html en sortie
+echo $html;
