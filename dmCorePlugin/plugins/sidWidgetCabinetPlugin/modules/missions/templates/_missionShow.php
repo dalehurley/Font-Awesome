fcfe3d9bd190986a3ee39f486cc808757a9512b3
Arnaud Gaudin <arnaudgaudin86@gmail.com>
2012-01-17 12:20:12 +0100

    Corrections affichage des missions

--- dmCorePlugin/plugins/sidWidgetCabinetPlugin/modules/missions/templates/_missionShow.php
+++ dmCorePlugin/plugins/sidWidgetCabinetPlugin/modules/missions/templates/_missionShow.php
@@ -1,5 +1,28 @@
 <?php
-if (count($missions)) { // si nous avons des actu articles
-    echo _tag('h2.title', $titreBloc);
-        include_partial("objectPartials/missionShow", array("mission" => $missions));
+$html = '';
+
+// si nous avons des actu articles
+if (count($missions)) {
+	
+	//titre du contenu
+	$html.= get_partial('global/titleWidget', array('title' => $titreBloc, 'isContainer' => true));
+	
+	/*
+	foreach ($missions as $mission) {
+		//ouverture du container
+		$html.= _open('div.supWrapper');
+		
+		//fermeture du container
+		$html.= _close('div.supWrapper');
+	}*/
+	
+	//affichage des missions
+	$pubOpts = array();
+	$pubOpts['node']		= $missions;
+
+	$html.= get_partial('global/publicationShow', $pubOpts);
+	
 } // sinon on affiche rien
+
+//affichage html de sortie
+echo $html;
--- dmCorePlugin/plugins/sidWidgetCabinetPlugin/modules/missions/templates/_missionsList.php
+++ dmCorePlugin/plugins/sidWidgetCabinetPlugin/modules/missions/templates/_missionsList.php
@@ -1,26 +1,42 @@
 <?php
 // vars : $missions, $nbMissions, $titreBloc, $longueurTexte
+$html = '';
+
 if (count($missions)) { // si nous avons des actu articles
+	
+	//gestion affichage du titre
     if($nbMissions == 1){
-        if ($titreBloc != true) {
-            echo _tag('h4.title', current($missions));
-        }
-        else  echo _tag('h4.title', $titreBloc);
+        if ($titreBloc != true) $html.= get_partial('global/titleWidget', array('title' => current($missions)));
+        else $html.= get_partial('global/titleWidget', array('title' => $titreBloc));
     }
     else {
-        if ($titreBloc != true) {
-            echo _tag('h4.title', __('The other missions of the office'));
-        }
-        else  echo _tag('h4.title', $titreBloc);
+        if ($titreBloc != true) $html.= get_partial('global/titleWidget', array('title' => __('The other missions of the office')));
+        else $html.= get_partial('global/titleWidget', array('title' => $titreBloc));
     }
     
-    echo _open('ul.elements');
+    //ouverture du listing
+    $html.= _open('ul.elements');
+	
+	//compteur
+	$count = 0;
+	$maxCount = count($missions);
+	
     foreach ($missions as $mission) {
-
-	include_partial("objectPartials/missionsList", array("article" => $mission,"longueurTexte" => $longueurTexte,"textEnd" => '(...)','titreBloc' => $titreBloc));
-
+		//incrémentation compteur
+		$count++;
+		
+		$html.= get_partial('global/publicationListElement', array(
+												'node' => $mission,
+												'count' => $count,
+												'maxCount' => $maxCount,
+												'teaserLength' => $longueurTexte,
+												'linkUrl' => $mission
+												));
     }
-    echo _close('ul');
-
-    
+	
+    //fermeture du listing
+    $html.= _close('ul.elements');
 } // sinon on affiche rien
+
+//affichage html de sortie
+echo $html;
