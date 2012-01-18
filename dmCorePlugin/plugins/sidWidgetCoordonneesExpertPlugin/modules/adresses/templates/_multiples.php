29002cf2c4ce0d08ea181381f8806cbb073d009c
Arnaud Gaudin <arnaudgaudin86@gmail.com>
2012-01-18 16:53:41 +0100

    Début de création d'un style pour les adresse de type organisation

--- dmCorePlugin/plugins/sidWidgetCoordonneesExpertPlugin/modules/adresses/templates/_multiples.php
+++ dmCorePlugin/plugins/sidWidgetCoordonneesExpertPlugin/modules/adresses/templates/_multiples.php
@@ -1,10 +1,40 @@
 <?php
 // vars : $adresses, $titreBloc
+$html = '';
+
 if (count($adresses)) {
-    if($titreBloc){// si nous avons des actu articles
-    echo _tag('h4.title', $titreBloc);
+	
+	//titre du contenu
+	if($titreBloc) $html.= get_partial('global/titleWidget', array('title' => $titreBloc));
+	
+	//ouverture du listing
+    $html.= _open('ul.elements');
+	
+	//compteur
+	$count = 0;
+	$maxCount = count($adresses);
+	
+    foreach($adresses as $adresse) {
+		//incrémentation compteur
+		$count++;
+		
+		$html.= get_partial('global/publicationListElement', array(
+												'node' => $adresse,
+												'itemType' => 'Organization',
+												'count' => $count,
+												'maxCount' => $maxCount
+												));
+		
+        //$html.= get_partial("objectPartials/adresseCabinet", array("renseignements" => $adresse));
     }
-    foreach($adresses as $adresse){
-        include_partial("objectPartials/adresseCabinet", array("renseignements" => $adresse));
-    }
+	
+    //fermeture du listing
+    $html.= _close('ul.elements');
+	
+	
+	
 } // sinon on affiche rien
+
+
+//affichage html de sortie
+echo $html;
