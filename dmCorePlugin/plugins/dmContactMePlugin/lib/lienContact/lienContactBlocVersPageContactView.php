dc739986fb1c72213c26f3a6e3d2d5f86d58d240
Arnaud Gaudin <arnaudgaudin86@gmail.com>
2012-01-18 11:57:45 +0100

    Ajout appel Css pour LienContactBlocVersPageContact

--- dmCorePlugin/plugins/dmContactMePlugin/lib/lienContact/lienContactBlocVersPageContactView.php
+++ dmCorePlugin/plugins/dmContactMePlugin/lib/lienContact/lienContactBlocVersPageContactView.php
@@ -11,6 +11,18 @@ class lienContactBlocVersPageContactView extends dmWidgetPluginView {
             'message'
         ));
     }
+	
+	public function getStylesheets() {
+		//on créé un nouveau tableau car c'est un nouveau widget (si c'est une extension utiliser $stylesheets = parent::getStylesheets();)
+		$stylesheets = array();
+		
+		//lien vers le js associé au menu
+		$cssLink = sfConfig::get('sf_css_path_template'). '/Widgets/LienContactBlocVersPageContact/LienContactBlocVersPageContact.css';
+		//chargement de la CSS si existante
+		if (is_file(sfConfig::get('sf_web_dir') . $cssLink)) $stylesheets[] = $cssLink;
+		
+		return $stylesheets;
+    }
 
     /**
      * On affiche NB articles Actu selon 3 types:
@@ -29,5 +41,5 @@ class lienContactBlocVersPageContactView extends dmWidgetPluginView {
             
                 ));        
 
-}
+	}
 }
