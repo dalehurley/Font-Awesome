5509705eaced2ab4012467d23d703bbb6474e976
lionel Fenneteau <lionel.fenneteau@gmail.com>
2012-01-23 17:06:18 +0100

    fixed: less param

--- dmCorePlugin/data/skeleton/web/less.php
+++ dmCorePlugin/data/skeleton/web/less.php
@@ -1,5 +1,7 @@
 <?php
 
+echo $_SERVER["REMOTE_ADDR"];
+
 //if (!in_array(@$_SERVER['REMOTE_ADDR'], array('127.0.0.1', '::1', '192.168.81.130')))
 if (substr($_SERVER['REMOTE_ADDR'], 0, 10) != '192.168.81') { // adresse locale dev
     if (!in_array(@$_SERVER['REMOTE_ADDR'], array('127.0.0.1','217.108.240.193', '217.108.240.209', '217.108.240.217', '217.108.240.225'))) {  // adresse externe
