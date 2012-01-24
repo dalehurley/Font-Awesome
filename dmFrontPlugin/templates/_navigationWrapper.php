c58ec38ccbe07c00d3fd7232463199c3670d92d6
Arnaud Gaudin <arnaudgaudin86@gmail.com>
2012-01-24 10:41:29 +0100

    Meilleur gestion du partial navigationWrapper

--- dmFrontPlugin/templates/_navigationWrapper.php
+++ dmFrontPlugin/templates/_navigationWrapper.php
@@ -1,71 +1,70 @@
 <?php
 /*
  * _navigationWrapper.php
- * v0.2
+ * v0.9
  * Permet d'afficher une navigation de page (à améliorer avec gestion intégrée des tableaux
  * 
  * Variables disponibles :
  * $placement	top ou bottom	
  * $elements	liens de navigation à afficher (html)
  * $pager		pager de type diem (html)
- * $isLight		indique une version simplifiée
+ * $container	container de l'ensemble
  * 
  */
 
-//Définitions des valeurs par défaut
+//Configuration par défaut
 
-//permet de ne pas être obligé de définir cette variable lorsque égale à false
-if(!isset($isLight)) $isLight = false;
+//container par défaut
+if(!isset($container)) $container = 'div';
 
-$ctnTag = $isLight ? 'span.navigation' : 'div.navigation';
-
+//déclaration des propriétés par défaut du container
+$ctnOpts = array('class' => 'navigation');
 switch ($placement) {
 	case 'top':
-		$ctnClass = '.navigationTop';
+		$ctnOpts['class'][] = 'navigationTop';
 		break;
 	case 'bottom':
-		$ctnClass = '.navigationBottom';
+		$ctnOpts['class'][] = 'navigationBottom';
 		break;
 	default:
-		$ctnClass = '';
 		break;
 }
 
-//ouverture container de la navigation
-$html = _open($ctnTag . $ctnClass);
+//Composition de la sortie html
+$html = '';
 
-	//liens de navigation multiples
-	if(isset($elements)){
-		$html.= _open('ul.elements');
-		
-		//compteur
-		$count = 0;
-		$maxCount = count($elements);
-		
-		foreach ($elements as $element) {
-			//incrémentation compteur
-			$count++;
-			//affichage du li contenant le lien
-			$html.= get_partial('global/publicationListElementLight', array(
-													'title' => $element['title'],
-													'linkUrl' => $element['linkUrl'],
-													'count' => $count,
-													'maxCount' => $maxCount
-													));
-		}
-		
-		$html.= _close('ul.elements');
-	}
-	
-	//Pager par défaut de Diem de type 1,2,3,4,5
-	if(isset($pager)){
-		$html.= _open('ul.pager');
-			$html.= $pager;
-		$html.= _close('ul.pager');
+//liens de navigation multiples
+if(isset($elements)){
+	$html.= _open('ul.elements');
+
+	//compteur
+	$count = 0;
+	$maxCount = count($elements);
+
+	foreach ($elements as $element) {
+		//incrémentation compteur
+		$count++;
+		//affichage du li contenant le lien
+		$html.= get_partial('global/publicationListElementLight', array(
+												'title' => $element['title'],
+												'linkUrl' => $element['linkUrl'],
+												'count' => $count,
+												'maxCount' => $maxCount
+												));
 	}
 
-//fermeture container de la navigation
-$html.= _close($ctnTag . $ctnClass);
+	$html.= _close('ul.elements');
+}
+
+//Pager par défaut de Diem de type 1,2,3,4,5
+if(isset($pager)){
+	$html.= _open('ul.pager');
+		$html.= $pager;
+	$html.= _close('ul.pager');
+}
+
+//englobage dans un container
+$html = _tag($container, $ctnOpts, $html);
 
 //affichage html en sortie
 echo $html;
