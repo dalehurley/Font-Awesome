505a69398c83ccad1f2a977a474040f388354a89
Arnaud Gaudin <arnaudgaudin86@gmail.com>
2012-02-07 16:18:02 +0100

    Ménage dans showArticle

--- dmCorePlugin/plugins/sidWidgetBePlugin/modules/article/templates/_showArticle.php
+++ dmCorePlugin/plugins/sidWidgetBePlugin/modules/article/templates/_showArticle.php
@@ -55,38 +55,8 @@ if ($doc_xml->load($xml)) {
 	
 	$html.= get_partial('global/schema/Thing/CreativeWork/Article', $articleOpts);
 	
-	/*
-    $return .= _tag('h2.title', $rubrique . ' - ' . $section);
-    $return .= '<article itemscope itemtype="http://schema.org/Article">';
-
-    //lien vers l'image
-    $imgLink = '/_images/lea' . $article->filename . '-g.jpg';
-    //on vérifie que l'image existe
-    $imgExist = is_file(sfConfig::get('sf_web_dir') . $imgLink);
-
-    // on teste si le fichier image est présent sur le serveur avec son chemin absolu
-    if ($imgExist) {
-        $return .= _open('div.imageFullWrapper');
-        $return .= _media($imgLink)
-                ->set('.image itemprop="image"')
-                ->alt($article->getTitle())
-                //redimenssionnement propre lorsque l'image sera en bibliothèque
-                ->width(spLessCss::gridGetContentWidth());
-        //->height(spLessCss::gridGetHeight(14,0))
-        $return .= _close('div');
-    }
-
-    $return .= _tag('h2.title itemprop="name"', $article->title);
-
-    
-    
-    $return .= $moteurXslt->transformToXML($doc_xml);
-
-//Fermeture de l'article
-    $return .= _close('article');
-	 */
 } else {
-    //$return = 'ERREUR : XML invalide :' . $xml;
+	$html.= debugTools::infoDebug(array(__('Error : invalid xml') => $xml),'warning');
 }
 
 //affichage html en sortie
