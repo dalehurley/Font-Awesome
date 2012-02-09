d2c04a70836f16560995fce40651d6026818bead
Arnaud Gaudin <arnaudgaudin86@gmail.com>
2012-02-09 12:32:18 +0100

    Création de nouveaux include séparés pour optimisation includes

---
+++ dmFrontPlugin/templates/_schema/_partialInitImage.php
@@ -0,0 +1,26 @@
+<?php
+/*
+ * _partialInitImage.php
+ * v1.0
+ * Permet d'initialiser l'image du partial
+ * 
+ * Variables disponibles :
+ * $image
+ * 
+ */
+
+//définition de l'image
+$isImage = false;
+if(isset($image)) {
+	//on vérifie si l'image est en base (de type objet dans ce cas)
+	$isDbImage = (gettype($image) == 'object') ? true : false;
+	
+	//si l'image est en base alors on rajoute le dossier upload devant pour obtenir l'url absolue
+	$imageUpload = ($isDbImage) ? sfConfig::get('sf_upload_dir') . '/' : sfConfig::get('sf_web_dir') . '/';
+	
+	//composition url absolue de l'image
+	$imageUrl = $imageUpload . $image;
+	
+	//test de la présence de l'image
+	$isImage = is_file($imageUrl);
+}
---
+++ dmFrontPlugin/templates/_schema/_partialInitValues.php
@@ -0,0 +1,36 @@
+<?php
+/*
+ * _partialInitValues.php
+ * v1.0
+ * Permet d'initialiser des variables du partial
+ * 
+ * Variables disponibles :
+ * $separator
+ * $dash
+ * $ellipsis
+ * $isListing			indique qu'il s'agit d'un listing
+ * $isLight				permet d'indiquer une version allégée (notamment pour des affichages spéciaux dans les sidebars)
+ * $noMicrodata			désactive tout ajout de microdata (désactivé par défaut)
+ * $isDateMeta
+ * 
+ */
+
+//récupération des différentes variables par défault
+$separator =  _tag('span.separator', sfConfig::get('app_vars-partial_separator'));
+$ellipsis = _tag('span.ellipsis', sfConfig::get('app_vars-partial_ellipsis'));
+$dash = _tag('span.dash', sfConfig::get('app_vars-partial_dash'));
+
+//longueur de texte de description par défaut
+$defaultValueLength = sfConfig::get('app_vars-partial_defaultValueLength');
+
+//permet de forcer la version listing de l'affichage
+if(!isset($isListing)) $isListing = false;
+
+//permet d'indiquer une version light de l'affichage (pour les listings simples)
+if(!isset($isLight)) $isLight = false;
+
+//désactive tout ajout de microdata
+if(!isset($noMicrodata)) $noMicrodata = false;
+
+//active l'affichage de la date en balise meta et désactive l'affichage des dates
+if(!isset($isDateMeta)) $isDateMeta = false;
