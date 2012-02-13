5cd96b913c0717d2e712dd188fc7ddf71705b279
Arnaud Gaudin <arnaudgaudin86@gmail.com>
2012-02-13 17:48:35 +0100

    Modifications champs et templates du widget socialNetwork

--- dmCorePlugin/plugins/sidWidgetSocialNetworkPlugin/lib/widget/socialNetworkLogosView.php
+++ dmCorePlugin/plugins/sidWidgetSocialNetworkPlugin/lib/widget/socialNetworkLogosView.php
@@ -11,14 +11,27 @@ class socialNetworkLogosView extends dmWidgetPluginView {
             'googleplus'
         ));
     }
+	
+	public function getStylesheets() {
+		//on créé un nouveau tableau car c'est un nouveau widget (si c'est une extension utiliser $stylesheets = parent::getStylesheets();)
+		$stylesheets = array();
+		
+		//lien vers le js associé au menu
+		$cssLink = sidSPLessCss::getCssPathTemplate(). '/Widgets/SocialNetworkLogos/SocialNetworkLogos.css';
+		//chargement de la CSS si existante
+		if (is_file(sfConfig::get('sf_web_dir') . $cssLink)) $stylesheets[] = $cssLink;
+		
+		return $stylesheets;
+	}
 
     protected function doRender() {
         $vars = $this->getViewVars();
 
         $arrayLogos = array(
-            'twitter' => $vars['twitter'],
             'facebook' => $vars['facebook'],
-            'googleplus' => $vars['googleplus']
+            'googleplus' => $vars['googleplus'],
+            'linkedin' => $vars['linkedin'],
+            'twitter' => $vars['twitter']
             );
 
        return $this->getHelper()->renderPartial('widget', 'socialNetworkLogos', array(
--- dmCorePlugin/plugins/sidWidgetSocialNetworkPlugin/modules/widget/templates/_socialNetworkLogos.php
+++ dmCorePlugin/plugins/sidWidgetSocialNetworkPlugin/modules/widget/templates/_socialNetworkLogos.php
@@ -1,7 +1,33 @@
 <?php
+$html = '';
+
+//ouverture du listing
+$html.= _open('ul.elements');
+
+//compteur
+$count = 0;
+$maxCount = count($logos);
 
 foreach ($logos as $logoName => $logoLink) {
-    echo _tag('p.logoSoicalNetwork',$logoName . ' - ' . $logoLink);
+	//incrémentation compteur
+	$count++;
+	
+	//options de l'élément
+	$elementOpt = array();
+
+	//gestion de l'index de positionnement
+	if($count == 1)			$elementOpt['class'][] = 'first';
+	if($count >= $maxCount)	$elementOpt['class'][] = 'last';
+	
+	//création du lien
+	$htmlLink = _link($logoLink)->text($logoName)->title(__('Follow us on') . ' ' . $logoName)->set('.link_' . dmString::slugify($logoName));
+	
+	//insertion du lien dans un li
+	$html.= _tag('li.element', $elementOpt, $htmlLink);
 }
 
+//fermeture du listing
+$html.= _close('ul.elements');
 
+//affichage html en sortie
+echo $html;
