7dcd104605bcd7fc6f528bcfe6ed57b4f7905184
Arnaud Gaudin <arnaudgaudin86@gmail.com>
2012-02-14 17:04:46 +0100

    Adaptation partial dmContactForm

--- dmCorePlugin/plugins/dmContactPlugin/modules/dmContact/templates/_form.php
+++ dmCorePlugin/plugins/dmContactPlugin/modules/dmContact/templates/_form.php
@@ -2,33 +2,39 @@
 // Contact : Form
 // Vars : $form
 
-if($sf_user->hasFlash('contact_form_valid'))
-{
-  echo _tag('p.form_valid', __('Thank you, your contact request has been sent.'));
-}
+//titre du contenu
+$html = get_partial('global/titleWidget', array('title' => __('ContactUs'), 'isContainer' => true));
 
-// open the form tag with a dm_contact_form css class
-echo $form->open();
+//ajout message de validation
+if($sf_user->hasFlash('contact_form_valid')) $html.= _tag('p.form_valid', __('Thank you, your contact request has been sent.'));
 
+//open the form tag with a dm_contact_form css class
+$html.= $form->open();
 
-   echo _tag('ul.dm_form_elements', $form['title']->renderRow() .
-            $form['name']->renderRow() .
-            $form['firstname']->renderRow() .
-            $form['function']->renderRow() .
-            $form['adresse']->renderRow() .
-            $form['postalcode']->renderRow() .
-            $form['ville']->renderRow() .
-            $form['email']->renderRow() .
-            $form['phone']->renderRow() .
-            $form['fax']->renderRow() .
-            $form['body']->renderRow()
-    );
+//ouverture du listing
+$html.= _open('ul.dm_form_elements');
+
+$html.= $form['title']->renderRow().
+		$form['name']->renderRow().
+		$form['firstname']->renderRow().
+		$form['function']->renderRow().
+		$form['adresse']->renderRow().
+		$form['postalcode']->renderRow().
+		$form['ville']->renderRow().
+		$form['email']->renderRow().
+		$form['phone']->renderRow().
+		$form['fax']->renderRow().
+		$form['body']->renderRow();
+
+//fermeture du listing
+$html.= _close('ul.dm_form_elements');
+
 
 // render captcha if enabled
 if($form->isCaptchaEnabled())
 {
 
-        echo '<script type="text/javascript">
+    $html.= '<script type="text/javascript">
        
         var RecaptchaOptions = {  
             custom_translations : {
@@ -51,14 +57,17 @@ if($form->isCaptchaEnabled())
         };
     </script>';
 
-  echo $form['captcha']->label('Captcha', 'for=false')->field()->error();
+  $html.= $form['captcha']->label('Captcha', 'for=false')->field()->error();
 
 }
 
-echo $form->renderHiddenFields();
+$html.= $form->renderHiddenFields();
 
 // change the submit button text
-echo _tag('div.submit_wrap', $form->submit(__('Send')));
+$html.= _tag('div.submit_wrap', $form->submit(__('Send')));
 
 // close the form tag
-echo $form->close();  
+$html.= $form->close();
+
+//affichage html en sortie
+echo $html;
