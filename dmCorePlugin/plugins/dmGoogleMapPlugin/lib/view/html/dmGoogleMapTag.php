bcf2a75f74dc1ca194306299b2e1f0d5280dfb27
stephane meron <smeron@lesechos.fr>
2012-02-15 15:02:08 +0100

    Débuggage de dmGoogleMapPlugin

--- dmCorePlugin/plugins/dmGoogleMapPlugin/lib/view/html/dmGoogleMapTag.php
+++ dmCorePlugin/plugins/dmGoogleMapPlugin/lib/view/html/dmGoogleMapTag.php
@@ -91,29 +91,13 @@ class dmGoogleMapTag extends dmHtmlTag
 
     $splash = $preparedAttributes['splash'];
     unset($preparedAttributes['splash']);
-	
-	//récupération de l'adresse en base
-	$adresseRequest = DmDb::table('SidCoordName')->findOneByIdAndIsActive($preparedAttributes['idCabinet'], true);
-	
-	//Ajout Arnaud : affichage de l'adresse
-	
-	//récupération du tiret
-	$dash = _tag('span.dash', sfConfig::get('app_vars-partial_dash'));
-	
-	//html de sortie
-	$html = '';
-	
-	//Si l'adresse à afficher est le siège social, alors on affiche le titreBloc
-	//Sinon on n'affiche rien dans le titreBloc car normalement la première adresse est tjrs celle du siège social
-	$isSiegeSocial = ($adresseRequest->siege_social == true);
-	$titreBloc = sfContext::getInstance()->getI18N()->__('Map');
-	if($isSiegeSocial) $html.= get_partial('global/titleWidget', array('title' => $titreBloc));
-	
-	//insertion de la carte GoogleMap
-    $html.= '<div'.$this->convertAttributesToHtml($preparedAttributes).'>'.$splash.'</div>';
-	
-	//composition des options du partial d'adresse
-	$addressOpts = array(
+    // initialisation des variables
+    $adresseCabinet = '';
+    $titreBloc ='';
+    //$adresseCabinet est l'adresse du cabinet récupéré en base
+
+    $adresseRequest = DmDb::table('SidCoordName')->findOneByIdAndIsActive($preparedAttributes['idCabinet'],true);
+    $addressOpts = array(
 					'name' => $adresseRequest->getTitle(),
 					'addressLocality' => $adresseRequest->getVille(),
 					'postalCode' => $adresseRequest->getCodePostal(),
@@ -126,10 +110,32 @@ class dmGoogleMapTag extends dmHtmlTag
 	if ($adresseRequest->getAdresse2() != NULL) $addressOpts['streetAddress'].= $dash . $adresseRequest->getAdresse2();
 	
 	//insertion du partial d'organization
-	$html.= get_partial('global/schema/Thing/Organization', $addressOpts);
-	
-	
-    return $html;
+	//$htmls= get_partial('global/schema/Thing/Organization', $addressOpts);
+    $cabinet = '';
+    $cabinet .= '<div xmlns="http://www.w3.org/1999/xhtml" itemtype="http://schema.org/Organization" itemscope="itemscope" class="mapAddress itemscope Organization"><span itemprop="name" class="itemprop name">'.$adresseRequest->getTitle().'</span>';
+    $adresseCabinet = $adresseRequest->getAdresse();
+    //vérification de adresse2
+    ($adresseRequest->getAdresse2() != NULL) ? $adresseCabinet .='-'.$adresseRequest->getAdresse2() : $adresseCabinet .='';
+    $cabinet .= '<div itemtype="http://schema.org/PostalAddress" itemscope="itemscope" class="address itemscope PostalAddress" itemprop="address"><span class="itemprop streetAddress"><span title="Rue" class="type">'.__("Street").'</span><span class="separator"> : </span><span itemprop="streetAddress" class="value">'.$adresseCabinet.'</span></span>';
+    $adresseCabinet .= ' - '.$adresseRequest->getCodePostal().' '.$adresseRequest->getVille();
+    $cabinet .= '<span class="subWrapper"><span class="itemprop postalCode"><span class="type" title="Postal Code">'.__("Postal Code").'</span><span class="separator">&nbsp;:&nbsp;</span><span class="value" itemprop="postalCode">'.$adresseRequest->getCodePostal().'</span>';
+    $cabinet .= '<span class="itemprop addressLocality"><span title="Localité" class="type">'.__("Locality").'</span><span class="separator"> : </span><span itemprop="addressLocality" class="value">'.$adresseRequest->getVille().'</span></span></span></div>';
+    // vérif si tél existe
+    ($adresseRequest->getTel() != NULL) ? $tel = '<p>Tél : '.$adresseRequest->getTel() : $tel = '';
+    ($adresseRequest->getTel() != NULL) ? $cabinet .= '<span class="itemprop telephone"><span title="Téléphone" class="type">Téléphone</span><span class="separator"> : </span><span itemprop="telephone" class="value">'.$adresseRequest->getTel().'</span></span>' : $cabinet .= '';
+    // vérif si fax existe
+    ($adresseRequest->getFax() !=NULL) ? $fax = ' - Fax : '.$adresseRequest->getFax().'</p>' : $fax = '</p>';
+    ($adresseRequest->getFax() !=NULL) ? $cabinet .= '<span class="itemprop faxNumber"><span class="type" title="Fax">Fax</span><span class="separator">&nbsp;:&nbsp;</span><span class="value" itemprop="faxNumber">'.$adresseRequest->getFax().'</span></span></div>' : $cabinet .= '';
+    // si l'adresse à afficher est le siège social, alors on affiche le titreBloc, 
+    //sinon on n'affiche rien dans le titreBloc car normalement la première adresse est tjrs celle du siège social
+    ($adresseRequest->siege_social == true ) ? $titreBloc = '<h2 class="title">'.  sfContext::getInstance()->getI18N()->__('Map').'</h2>' : $titreBloc ='' ;
+    // construction de la chaîne html
+    
+
+    //$tag = $titreBloc.'<div style="text-align: center"><p><b>'.$adresseRequest->getTitle().'</b>'.$cabinet.'<br />'.$adresseCabinet.'</p>'.$tel.$fax.'</div><div'.$this->convertAttributesToHtml($preparedAttributes).'>'.$splash.'</div>';
+    $tag = $titreBloc.'<div'.$this->convertAttributesToHtml($preparedAttributes).'>'.$splash.'</div><div class="mapAdress">'.$cabinet.'</div>';
+    
+    return $tag;
   }
 
   protected function prepareAttributesForHtml(array $attributes)
@@ -144,7 +150,6 @@ class dmGoogleMapTag extends dmHtmlTag
     foreach($this->getJsonAttributes() as $jsonAttribute)
     {
       $jsonAttributes[$jsonAttribute] = dmArray::get($attributes, $jsonAttribute);
-
       unset($attributes[$jsonAttribute]);
     }
 
