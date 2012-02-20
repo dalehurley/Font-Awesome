b1699d5636d1d621952774db3cba9965d32d1f5a
stephane meron <smeron@lesechos.fr>
2012-02-20 14:30:09 +0100

    réécriture du script pour gestion des class first et last

--- dmCorePlugin/plugins/sidWidgetActuPlugin/modules/actusDuCabinet/templates/_actuArticlesContextuel.php
+++ dmCorePlugin/plugins/sidWidgetActuPlugin/modules/actusDuCabinet/templates/_actuArticlesContextuel.php
@@ -2,44 +2,56 @@
 // vars : $articles, $nbArticles, $titreBloc, $lien, $length, $chapo, $width, $height
 
 $i = 1;
+$position = '';
 if (count($articles)) { // si nous avons des actu articles
 	
 	
 	
 	//gestion affichage du titre
     echo _tag('h4.title',$titreBloc);
-?>
-<ul class="elements">
-<?php		
+
+    echo _open('ul', array('class' => 'elements'));
+    
 	foreach ($articles as $article) {  
-        $link = '';    ?>
-       <li class="element itemscope Article first last" itemtype="http://schema.org/Article" itemscope="itemscope">
+        $link = '';    
         
-       <?php
+        switch ($i){
+            case $i_max = 1: $position = 'first last';
+                break;
+            case '1' : $position = 'first';
+                break;
+            case $i_max : $position = 'last';
+                break;
+            default : $position = '';
+        }
+       
+        
+        echo _open('li', array('class' => 'element itemscope Article '.$position, 'itemtype' => 'http://schema.org/Article' , 'itemscope' => 'itemscope'));
         
         if ($withImage == true) {
             if (($article->getImage() != NULL) and ($i <= sfConfig::get('app_nb-image'))) {
-                $link .= '<span class="imageWrapper">';
-                $link .= _media($article->getImage())->width($width)->set('.image');
-                $link .= '</span>';
+                $link .= _open('span', array('class' => 'imageWrapper'));
+                $link .= _media($article->getImage())->width($width)->set('.image')->alt($article->getTitle());
+                $link .= _close('span');
             }
         };
-        $link .='<span class="wrapper">
-                <span class="subWrapper">';
+        $link .= _open('span' , array('class' => 'wrapper'));
+                    _open('span' , array('class' => 'subWrapper'));
 
-                   if ($titreBloc != $article->getTitle()) {
-                       $link .= '<span class="title itemprop name" itemprop="name">' . $article->getTitle() . '</span>';
-                   };
-                   $link .= '<meta content="' . $article->createdAt . '" itemprop="datePublished">
-                </span>
-                <span class="teaser itemprop description" itemprop="description">';
+                       if ($titreBloc != $article->getTitle()) {
+                           $link .= _tag('span', array('class' => 'title itemprop name', 'itemprop' => 'name') , $article->getTitle());
+                       };
+                       $link .= _tag('meta' , array('content' => $article->createdAt, 'itemprop' => 'datePublished'));
+                    $link .= _close('span');
+                    $link .= _open('span', array('class' =>'teaser itemprop description' , 'itemprop' => 'description'));
                    if ($chapo == 0) {
                        $link .= stringTools::str_truncate($article->getResume(), $length, '(...)', true);
                    } 
                    else if ($chapo == 1) {
                        $link .= $article->getText();
                    }
-                   $link .= '</span></span>';
+                   $link .= _close('span');
+               $link .= _close('span');
       
        echo _link($article)->set('.link_box')->text($link); 
        $i++;   
@@ -49,14 +61,13 @@ if (count($articles)) { // si nous avons des actu articles
     } ?>
 </ul>
 <?php
-if ((isset($lien)) AND ($lien != '')) { ?>
-        <div class="navigationWrapper navigationBottom">
-            <ul class="elements">
-                <li class="element first last">
-                    <?php echo _link('sidActuArticle/list')->set('.link_box')->text($lien); ?>
-                </li>
-        </ul>
-        </div>
-        <?php
+if ((isset($lien)) AND ($lien != '')) { 
+        echo _open('div', array('class' => 'navigationWrapper navigationBottom'));
+            echo _open('ul', array('class' => 'elements'));
+                echo _tag('li', array('class' => 'element'), 
+                        _link('sidActuArticle/list')->set('.link_box')->text($lien)
+                        );
+        echo _close('div');
+    
     }
 } // sinon on affiche rien
