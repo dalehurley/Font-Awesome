0189ebca9e2622bd4047e453143165b270673725
lionel Fenneteau <lionel.fenneteau@gmail.com>
2012-02-27 17:58:13 +0100

    ajout liste des articles de la meme section pour les articles de datatype AGENDA

--- dmCorePlugin/plugins/sidWidgetBePlugin/modules/affichage/templates/_article.php
+++ dmCorePlugin/plugins/sidWidgetBePlugin/modules/affichage/templates/_article.php
@@ -2,21 +2,10 @@
 
 // Vars: $article
 
-
-$xml = sfConfig::get('app_rep-local') .
-        $article->getSection()->getRubrique() .
-        '/' .
-        $article->getSection() .
-        '/' .
-        $article->filename .
-        '.xml';
-$xsl = dm::getDir() . '/dmCorePlugin/plugins/sidWidgetBePlugin/lib/xsl/' . sfConfig::get('app_xsl-article');
-
 if (!is_file($xml)) {
     echo debugTools::infoDebug(array(__('Error : missed file') => $xml), 'warning');
 }
 
-$dataType = xmlTools::getLabelXml($xml, "DataType");
 echo debugTools::infoDebug(array('id LEA' => $article->filename, 'DataType' => $dataType));
 
 // traitement des dossiers
@@ -29,7 +18,7 @@ if ($dataType == 'DOSSIER'){
             ));
     $pageSession = sfContext::getInstance()->getUser()->setAttribute('articleDataType', sfConfig::get('app_article-data-type-dossier'));
 }
-elseif (($dataType == 'ARTICLE') || $dataType == 'AGENDA'){
+elseif (($dataType == 'ARTICLE')){
     include_partial('article/showArticle', 
         array(
             'article' => $article,
@@ -38,9 +27,20 @@ elseif (($dataType == 'ARTICLE') || $dataType == 'AGENDA'){
             ));
     $pageSession = sfContext::getInstance()->getUser()->setAttribute('articleDataType', sfConfig::get('app_article-data-type-article'));
 }
+elseif ($dataType == 'AGENDA'){ // on affiche l'article de dataType AGENDA suivi de tous les autres articles du mois
+    include_partial('article/showArticle', 
+        array(
+            'article' => $article,
+            'withImage' => $withImage,
+            'widthImage' => $widthImage,
+            'articleList' => $articleList
+            ));
+    $pageSession = sfContext::getInstance()->getUser()->setAttribute('articleDataType', sfConfig::get('app_article-data-type-article'));
+}
 else {
     // ni DOSSIER ni ARTICLE 
 }
+
 
 
 
