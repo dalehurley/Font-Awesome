8f655c22d41f1c6f10a4dda5cd5968238ff45609
stephane meron <smeron@lesechos.fr>
2012-02-28 09:39:08 +0100

    modification de dmWidgetFeedReader pour gérer le titre et longueur de texte

---
+++ dmCorePlugin/plugins/dmWidgetFeedReaderPlugin/modules/dmWidget/templates/_dmWidgetFeedReaderShowForm.php
@@ -0,0 +1,22 @@
+<?php
+
+echo _tag('ul.help',
+	_tag('li',"Ce widget permet d'afficher les flux rss")
+	);
+
+
+echo
+
+$form->renderGlobalErrors(),
+
+
+_tag('div',
+    $form['url']->renderRow().
+    $form['title']->renderRow().	
+    $form['nbArticles']->renderRow().
+    $form['length']->renderRow().
+    $form['life_time']->renderRow().
+    $form['cssClass']->renderRow()
+         
+  
+); 
