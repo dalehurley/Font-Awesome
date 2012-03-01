e69def8bc543da95bdc28a1fa081b62f06c891f8
Arnaud Gaudin <arnaudgaudin86@gmail.com>
2012-03-01 16:21:16 +0100

    Désactivation débug temporaire des wrappers

--- dmCorePlugin/plugins/sfLESSPlugin/data/_framework/SPLessCss/Externals/js/modernizr/polyfills.js
+++ dmCorePlugin/plugins/sfLESSPlugin/data/_framework/SPLessCss/Externals/js/modernizr/polyfills.js
@@ -1,6 +1,6 @@
 // polyfills.js
-// v0.1
-// Last Updated : 2011-10-10 16:20
+// v0.5
+// Last Updated : 2012-03-01 15:50
 // Copyright : SID Presse
 // Author : Arnaud GAUDIN
 
@@ -10,45 +10,81 @@
 	//lancement lorsque le document est chargé
 	$(document).ready(function(){
 		
+		//on vérifie dans quel mode on est
+		var isDev = $('body').hasClass('isDev');
+		var isLess = $('body').hasClass('isLess');
+
 		//variable permettant de détecter la valeur de touch screen de l'écran
 		var isTouchDevice = 'ontouchstart' in document.documentElement;
-		
-		if(isTouchDevice){
-			/*
-			$("*").live("touchstart", function() {
-				$(this).addClass("touch");
-				}).live("touchend", function() {
-					$(this).removeClass("touch");
-				});*/
-			/*	
-			$(".debugTemplate").live("tap", function() {
-				$(this).addClass("touch");
-			});*/
-			/*
-			$(".debugTemplate").live("tap taphold swipe swipeleft swiperight", function(e) {
-				$(this).toggleClass(e.type);
+
+		//Définition hauteur bl
+		var bH = 18;
+		$('.imageWrapper, .imageFullWrapper').each(function(index){
+			//ciblage du wrapper
+			var wrapper = this;
+			//récupération de l'image
+			var image = $(wrapper).find("img");
+
+			//application au chargement de l'image
+			$(image).load(function(){
+				//dimenssions de l'image
+				var imgWidth = $(image).width();
+				var imgHeight = $(image).height();
+				
+				//on récupère le nbre le plus proche
+				var nbreBl = Math.floor(imgHeight / bH);
+
+				//on rajoute +1 en mode Less pour bien voir le décalage
+				if(isLess) nbreBl += 1;
+
+				//calcul hauteur rectifiée
+				var getHeight = nbreBl * bH;
+
+				//calcul marge de décalage de l'image
+				var decalMarginTop = (getHeight - imgHeight) / 2;
+
+				//application hauteur sur imageWrapper et marginTop sur l'image uniquement si différence de hauteur
+				if(getHeight != imgHeight) {
+					$(wrapper).height(getHeight);
+					$(image).css('marginTop', decalMarginTop);
+					//$(this).animate({height:getHeight}, 1000);
+					//$(image).animate({marginTop:decalMarginTop}, 1000);
+				}
+
+				//affichage dans console de débug
+				window.console.log("#" + index + " alt : " + $(image).attr('alt')  + " | " + imgWidth + " x " + imgHeight + " | nbreBl : " + nbreBl + " | getHeight : " + getHeight + " | decalMarginTop : " + decalMarginTop);
 			});
-			*/
-		}
-		
-		/*
-		if(Modernizr.touch) {
-			$("*").live("touchstart", function() {
-				$(this).addClass("touch");
-				}).live("touchend", function() {
-					$(this).removeClass("touch");
-				});
-			$('*').bind('touchstart', function() {
-				$(this).addClass("touch");
-			});
-			$('*').bind('touchend', function() {
-				$(this).removeClass("touch");
-			});
-		}else {
-			
-		}
-		*/
-		
+		});
+
+		//test diverses d'utilisation du touch
+		// if(isTouchDevice){
+		// 	$("*").live("touchstart", function() {
+		// 		$(this).addClass("touch");
+		// 		}).live("touchend", function() {
+		// 			$(this).removeClass("touch");
+		// 		});
+		// 	$(".debugTemplate").live("tap", function() {
+		// 		$(this).addClass("touch");
+		// 	});
+		// 	$(".debugTemplate").live("tap taphold swipe swipeleft swiperight", function(e) {
+		// 		$(this).toggleClass(e.type);
+		// 	});
+		// }
+
+		// if(Modernizr.touch) {
+		// 	$("*").live("touchstart", function() {
+		// 		$(this).addClass("touch");
+		// 		}).live("touchend", function() {
+		// 			$(this).removeClass("touch");
+		// 		});
+		// 	$('*').bind('touchstart', function() {
+		// 		$(this).addClass("touch");
+		// 	});
+		// 	$('*').bind('touchend', function() {
+		// 		$(this).removeClass("touch");
+		// 	});
+		// }else {
+		// }
 	});
 	
 })(jQuery);
