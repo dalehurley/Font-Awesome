4ac55a682666ba8e447db3694c76c77b8d210a50
stephane meron <smeron@lesechos.fr>
2012-03-02 14:14:51 +0100

    ajout des champs debut_date et fin_date pour gérer la publication sur le site

--- dmCorePlugin/plugins/sidWidgetActuPlugin/lib/actusDuCabinet/actusDuCabinetActuArticlesListView.php
+++ dmCorePlugin/plugins/sidWidgetActuPlugin/lib/actusDuCabinet/actusDuCabinetActuArticlesListView.php
@@ -36,10 +36,10 @@ class actusDuCabinetActuArticlesListView extends dmWidgetPluginView {
                 
                 $actuArticles = Doctrine_Query::create()
                         ->from('SidActuArticle a')
-                            ->leftJoin('a.Translation b')
+                        ->withI18n(sfContext::getInstance()->getUser()->getCulture(), null, 'a')
                         ->leftJoin('a.SidActuTypeArticle sata')
                         ->where('a.is_active = ? and sata.sid_actu_type_id = ? and a.id <> ?', array(true, $vars['type'], $dmPage->record_id))
-                        ->orderBy('b.updated_at DESC')
+                        ->orderBy('aTranslation.updated_at DESC')
                         ->limit($nbArticles)
                         ->execute();
 
@@ -52,10 +52,10 @@ class actusDuCabinetActuArticlesListView extends dmWidgetPluginView {
                 
                 $actuArticles = Doctrine_Query::create()
                         ->from('SidActuArticle a')
-                            ->leftJoin('a.Translation b')
+                        ->withI18n(sfContext::getInstance()->getUser()->getCulture(), null, 'a')
                         ->leftJoin('a.SidActuTypeArticle sata')
                         ->where('a.is_active = ? and sata.sid_actu_type_id = ?', array(true, $vars['type']))
-                        ->orderBy('b.updated_at DESC')
+                        ->orderBy('aTranslation.updated_at DESC')
                         ->limit($nbArticles)
                         ->execute();
 
