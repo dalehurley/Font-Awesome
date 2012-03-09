9e3b17ba3f13e303b3fbca2e42ca34653a102e07
lionel Fenneteau <lionel.fenneteau@gmail.com>
2012-03-09 16:39:58 +0100

    delete _old_themesFMK dir

--- _old_themesFmk/_templates/BaseTheme/Externals/php/layouts/customSidebarRightSuccess.php
+++
@@ -1,8 +0,0 @@
-<?php
-//Personnalisation de la mise en page par défaut
-$pageOptionsCustom['areas']['dm_sidebar_right']['isPage'] = true;
-
-//Récupération et chargement du template de page
-$pSTInc = spLessCss::pageSuccessTemplateInclude();
-if($pSTInc['isFile'])	include	$pSTInc['include'];
-else					echo	$pSTInc['errorMsg'];
