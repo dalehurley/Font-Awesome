ae00486831ebc3f35e6d82855165ae3cdd6744cd
lionel Fenneteau <lionel.fenneteau@gmail.com>
2012-03-22 17:48:57 +0100

    Maj navigation html code

--- dmFrontPlugin/modules/dmWidget/templates/_dmWidgetSearchResults.php
+++ dmFrontPlugin/modules/dmWidget/templates/_dmWidgetSearchResults.php
@@ -12,21 +12,46 @@ if (!$pager)
   return;
 }
 
-echo _tag('h1', __('Results %1% to %2% of %3%', array(
+echo _tag('h2.title', __('Results %1% to %2% of %3%', array(
   '%1%' => $pager->getFirstIndice(),
   '%2%' => $pager->getLastIndice(),
   '%3%' => $pager->getNbResults()
 )));
 
-echo $pager->renderNavigationTop();
+if ($pager->getOption('navigation_top')){
+  echo _tag('div.navigation.navigationTop',
+      $pager->renderNavigationTop()
+    );
+} else {
+  echo $pager->renderNavigationTop();
+}
 
 echo _open('ol.search_results start='.$pager->getFirstIndice());
 
+
+  //compteur
+  $i = 1;
+  $i_max = $pager->getLastIndice() - $pager->getFirstIndice() +1;
+
 foreach($pager as $result)
 {
+
+  $position = '';
+    switch ($i){
+        case '1' : 
+          if ($i_max == 1) $position = '.first.last';
+          else $position = '.first';
+            break;
+        default : 
+          if ($i == $i_max) $position = '.last';
+          else $position = '';
+          break;
+    }
+    $i++;
+
   $page = $result->getPage();
   
-  echo _tag('li.search_result',
+  echo _tag('li.search_result'.$position,
   
     _tag('span.score', ceil(100*$result->getScore()).'%').
     
@@ -44,4 +69,10 @@ foreach($pager as $result)
 
 echo _close('ol');
 
-echo $pager->renderNavigationBottom();
+if ($pager->getOption('navigation_bottom')){
+  echo _tag('div.navigation.navigationBottom',
+      $pager->renderNavigationBottom()
+    );
+} else {
+  echo $pager->renderNavigationBottom();
+}
