468d6ac7100242d9fee0992bc4e6a66829a651bc
stephane meron <smeron@lesechos.fr>
2012-03-23 09:24:20 +0100

    maj pour : - Equipe : autorisation pour le client de rajouter photo de ses collaborateurs - Mission / Recrutement / PageCabinet : ajout de téléchargement de fichier (.pdf, .doc, .docx, .xls, .xlsx, .odt, .ods) et d'image

--- dmCorePlugin/plugins/sidWidgetCabinetPlugin/modules/recrutementAdmin/lib/SidCabinetRecrutementAdminForm.php
+++ dmCorePlugin/plugins/sidWidgetCabinetPlugin/modules/recrutementAdmin/lib/SidCabinetRecrutementAdminForm.php
@@ -22,27 +22,31 @@ class SidCabinetRecrutementAdminForm extends BaseSidCabinetRecrutementForm
     
     protected function createMediaFormForFile() {
 	$form = parent::createMediaFormForFile();
-        
         unset($form['legend'], $form['author'], $form['license']); 
 
 	// choose mime types allowed
 	$form->setMimeTypeWhiteList(array(
-	    'application/pdf',
+	    'application/pdf', // .pdf
 	    'application/msword', // .doc
 	    'application/vnd.oasis.opendocument.text', // .odt
-	    'web_images'
+	    'web_images',
+            'application/zip', // .zip
+            'application/vnd.openxmlformats-officedocument.wordprocessingml.document', // .docx
+            'application/vnd.ms-excel', // .xls
+            'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet', // .xlsx
+            'application/vnd.oasis.opendocument.spreadsheet', // .ods
+            
 	));
-
-
-	return $form;
+        return $form;
     }
+    
     public function setup() {
         parent::setup();
         $this->widgetSchema['title_file']->setAttributes(array('class' => 'input_short'));
         $this->widgetSchema->setHelps(array(
 //            'title_entete_page' => '<b>Cet intitulé sera le titre de la page (100 caractères MAXI)</b>',
             'text' => '<u>Information</u>: <br /> - pour un retour à la ligne : faire Maj + ENTREE en même temps <br /> - pour un nouveau paragraphe : faire ENTREE',
-            'file_form' => 'Vous pouvez insérer un fichier des formats suivants : Pdf, Word, OpenOffice.',
+            'file_form' => 'Vous pouvez insérer un fichier des formats suivants : .pdf, doc, .docx, .xls, .xlsx, .odt, .ods, .zip.',
             'title_file' => 'Nouveau nom de votre fichier',));
         
         }
