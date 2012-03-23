675f3c65568dea8bac8a1d9d2f5f78a04dc43223
lionel Fenneteau <lionel.fenneteau@gmail.com>
2012-03-23 12:16:22 +0100

    delete useless webservices modules

---
+++ dmCorePlugin/plugins/sidWebservicesPlugin/modules/va/actions/components.class.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * webservices components
+ * 
+ * No redirection nor database manipulation ( insert, update, delete ) here
+ */
+class vaComponents extends myFrontModuleComponents
+{
+
+  public function executeForm(dmWebRequest $request)
+  {
+    // Your code here
+  }
+
+
+}
