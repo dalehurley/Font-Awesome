8229b64266cb3efdac43d84160f8a4d6145378d1
lionel Fenneteau <lionel.fenneteau@gmail.com>
2012-04-03 14:28:20 +0200

    maj message

--- dmFrontPlugin/lib/config/dmFrontApplicationConfiguration.php
+++ dmFrontPlugin/lib/config/dmFrontApplicationConfiguration.php
@@ -44,6 +44,12 @@ abstract class dmFrontApplicationConfiguration extends dmApplicationConfiguratio
     $message = '';
     $contactFields = get_object_vars($contact);
     $message .= count($contactFields);
+
+$message .= $contact->body;
+$message .= $contact->email;
+$message .= $contact->name;
+
+
     foreach ($contactFields as $key => $value) {
       $message .= $key .":". $value."
 ";
