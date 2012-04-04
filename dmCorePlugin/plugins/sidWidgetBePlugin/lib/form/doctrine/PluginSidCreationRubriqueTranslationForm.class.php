7b5ac39ca9fa2e737414a9be6c50a5b8aedc4983
stephane meron <smeron@lesechos.fr>
2012-04-04 10:04:41 +0200

    test crea

---
+++ dmCorePlugin/plugins/sidWidgetBePlugin/lib/form/doctrine/PluginSidCreationRubriqueTranslationForm.class.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * PluginSidCreationRubriqueTranslation form.
+ *
+ * @package    ##PROJECT_NAME##
+ * @subpackage form
+ * @author     ##AUTHOR_NAME##
+ * @version    SVN: $Id$
+ * @generator  Diem 5.4.0-DEV
+ * @gen-file   /data/www/_lib/diem/dmCorePlugin/data/generator/dmDoctrineForm/default/template/sfDoctrineFormPluginTemplate.php */
+abstract class PluginSidCreationRubriqueTranslationForm extends BaseSidCreationRubriqueTranslationForm
+{
+  public function setup()
+  {
+    parent::setup();
+    /*
+     * Here, the plugin form code
+     */
+  }
+}
