5343d77be292a5082e385d2449fa8c4df18212bf
stephane meron <smeron@lesechos.fr>
2012-04-12 11:34:09 +0200

    remise à page:true dans le module.yml et mise en commentaire du code dans les fichiers _show et components (module pas afficher directement sur les pages)

--- dmCorePlugin/plugins/sidWidgetCoordonneesExpertPlugin/modules/renseignements/actions/components.class.php
+++ dmCorePlugin/plugins/sidWidgetCoordonneesExpertPlugin/modules/renseignements/actions/components.class.php
@@ -4,18 +4,18 @@
  * 
  * No redirection nor database manipulation ( insert, update, delete ) here
  */
-class renseignementsComponents extends myFrontModuleComponents
-{
-
-  public function executeShow()
-  {
-    $query = $this->getShowQuery();
-    
-    $this->renseignements = $this->getRecord($query);
-    	$idDmPage = sfContext::getInstance()->getPage()->id;
-	$dmPage = dmDb::table('DmPage')->findOneById($idDmPage);
-      $this->page =  $dmPage->module.'/'.$dmPage->action.' - '.$dmPage->record_id;
-  }
-
-
-}
+//class renseignementsComponents extends myFrontModuleComponents
+//{
+//
+//  public function executeShow()
+//  {
+//    $query = $this->getShowQuery();
+//    
+//    $this->renseignements = $this->getRecord($query);
+//    	$idDmPage = sfContext::getInstance()->getPage()->id;
+//	$dmPage = dmDb::table('DmPage')->findOneById($idDmPage);
+//      $this->page =  $dmPage->module.'/'.$dmPage->action.' - '.$dmPage->record_id;
+//  }
+//
+//
+//}
