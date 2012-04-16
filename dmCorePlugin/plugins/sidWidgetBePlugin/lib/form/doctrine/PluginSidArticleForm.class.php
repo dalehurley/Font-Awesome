129b3d4ded990d7c6854a1959e46edd4a41f07cc
stephane meron <smeron@lesechos.fr>
2012-04-16 15:13:57 +0200

    modif pour afficher la photo d'un dossier et/ou pas de chapeau

--- dmCorePlugin/plugins/sidWidgetBePlugin/lib/form/doctrine/PluginSidArticleForm.class.php
+++ dmCorePlugin/plugins/sidWidgetBePlugin/lib/form/doctrine/PluginSidArticleForm.class.php
@@ -14,15 +14,4 @@ abstract class PluginSidArticleForm extends BaseSidArticleForm
   {
     parent::setup();
 	}
-
-	public function configure(){
-		parent::configure();
-    $this->widgetSchema['widthImage'] = new sfWidgetFormInputText(array('label' => 'Largeur en px'));
-    $this->validatorSchema['widthImage'] = new dmValidatorCssSize(array(
-                  'required' => false
-                ));
-
-    $this->widgetSchema['withImage'] = new sfWidgetFormInputCheckbox(array('default'=> true, 'label' => 'Image visible dans les articles'));
-    $this->validatorSchema['withImage']  = new sfValidatorBoolean();
-  }
 }
