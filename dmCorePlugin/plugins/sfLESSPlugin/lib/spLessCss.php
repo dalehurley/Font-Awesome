c111f2f51848ba422cba10f145dd92630c8357db
Arnaud Gaudin <arnaudgaudin86@gmail.com>
2012-04-17 17:44:56 +0200

    Appel des fichiers JS de gestion des thèmes depuis la classe spLessCss

--- dmCorePlugin/plugins/sfLESSPlugin/lib/spLessCss.php
+++ dmCorePlugin/plugins/sfLESSPlugin/lib/spLessCss.php
@@ -12,6 +12,11 @@ class spLessCss {
 			//affichage du widget de DEBUG du framework
 			echo dm_get_widget('debug', 'debug', array());
 		}
+
+		//lien vers le js associé au theme
+		$jsLink = '/theme/less/_templates/' . dmConfig::get('site_theme'). '/Externals/js/theme/frontTheme.js';
+		//chargement de la CSS si existante
+		if (is_file(sfConfig::get('sf_web_dir') . $jsLink)) use_javascript($jsLink);
 		
 		//actualisation des paramètres du framework
 		//if($pageOptions['isLess']) sidSPLessCss::loadLessParameters();
