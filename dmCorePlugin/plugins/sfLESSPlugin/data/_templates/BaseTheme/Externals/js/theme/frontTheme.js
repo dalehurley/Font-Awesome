66473c87033148e40fa8d214e842541ba14ac079
Arnaud Gaudin <arnaudgaudin86@gmail.com>
2012-04-17 17:43:55 +0200

    Tous les thèmes : création des fichier JS de gestion du front

---
+++ dmCorePlugin/plugins/sfLESSPlugin/data/_templates/BaseTheme/Externals/js/theme/frontTheme.js
@@ -0,0 +1,31 @@
+// frontTheme.js
+// v1.0
+// Last Updated : 2012-04-17 17:30
+// Copyright : SID Presse
+// Author : Arnaud GAUDIN
+
+//permet d'isoler le code du reste de l'environnement javascript
+(function($){
+
+	//Définition du plugin
+	$.fn.frontTheme = function() {
+
+		// iterate and reformat each matched element
+		return this.each(function() {
+			//Affichage debug initialisation
+			$.fn.menuMegaDropdown.debug("frontTheme BaseTheme | initialisation");
+		});
+	}
+
+	//fonction de debuggage
+	$.fn.frontTheme.debug = function(txt){
+		if (window.console && window.console.log)
+			window.console.log(txt);
+	}
+
+	//lancement automatique de la fonction
+	$(document).ready(function(){
+		$('html').frontTheme();
+	});
+
+})(jQuery);
