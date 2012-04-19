4cb7d8cb81c5cad0a79323a1b3322e81a8ed7161
Arnaud Gaudin <arnaudgaudin86@gmail.com>
2012-04-19 12:48:09 +0200

    Maestro : avancement script JS de placement du thème

--- dmCorePlugin/plugins/sfLESSPlugin/data/_templates/maestroTheme/Externals/js/theme/frontTemplate.js
+++ dmCorePlugin/plugins/sfLESSPlugin/data/_templates/maestroTheme/Externals/js/theme/frontTemplate.js
@@ -28,32 +28,38 @@
 	$.fn.frontTemplate.resizeCols = function (options) {
 
 		//changement des valeurs des options
-
 		//on vérifie la présence de différents widgets à placer en bas dans la sidebarLeft
 
 		//sélection de diverses widgets à placer en bas de la colonne
-		var widgetFollowingMenu = $('#dm_sidebar_left .dm_widget.navigation_menu ~ .dm_widget.social_network_logos');
+		// var widgetFollowingMenu = $('#dm_sidebar_left > .dm_zones > .dm_zone > .dm_widgets > .dm_widget.navigation_menu ~ .dm_widget:not(.navigation_menu)');
+		// notation ne nécessitant pas de parser, donc plus rapide
+		// cf. http://aahacreative.com/2010/07/19/jquery-fastest-method-find-descendents
+		var widgetFollowingMenu = $('#dm_sidebar_left').find('.dm_widget.navigation_menu').siblings('.dm_widget').not('.navigation_menu');
 
-		$.fn.frontFramework.debug("widgetFollowingMenu : " + widgetFollowingMenu.length);
-		//à refaire
-		/*
-		if(widgetSocialNetworkLogos.length > 0) {
-			//on soustrait la hauteur au calcul à posteriori
-			options.isPostHSL = true;
-			options.offsetHSL -= widgetSocialNetworkLogos.outerHeight(true);
+		//initialisation hauteur totale
+		var fullHeight = 0;
+
+		//on parcourt la sélection de widgets
+		widgetFollowingMenu.each(function(index) {
+			//incrémentation hauteur courante (marge comprise)
+			fullHeight += $(this).outerHeight(true);
 
 			//replacement en absolu du widget
-			widgetSocialNetworkLogos.css({
+			$(this).css({
 										'position' : 'absolute',
-										'bottom' : '0'
+										'bottom' : (fullHeight*-1)
 									});
-			// $.fn.frontFramework.debug("widgetSocialNetworkLogos outerHeight : " + widgetSocialNetworkLogos.outerHeight(true));
-		}
-		*/
 
-		//ajout d'un padding bottom à l'area pour placer l'élément
-		//$('#dm_sidebar_left').css('paddingBottom', Math.abs(options.offsetHSL));
+			// $.fn.frontFramework.debug(index + " widget : " + $(this).attr('class') + ' fullHeight ' + fullHeight + ' outerHeight ' + $(this).outerHeight(true));
+		});
 
+		//on ajoute du padding bottom à la sidebar pour loger les widgets précisemment, et on change le position des zones
+		$('#dm_sidebar_left').css('paddingBottom', fullHeight).children('.dm_zones').css('position', 'relative');
+
+		//on soustrait la hauteur au calcul à posteriori
+		options.isPostHSL = true;
+		options.offsetHSL -= fullHeight;
+		// $.fn.frontFramework.debug("widgetFollowingMenu fullHeight : " + fullHeight);
 		//appel de la fonction de redimenssionnement générale
 		$.fn.frontFramework.resizeCols(options);
 	}
