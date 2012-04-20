a3419b760a09f85366f1e4a857395310d8524382
lionel Fenneteau <lionel.fenneteau@gmail.com>
2012-04-20 13:49:36 +0200

    add comment to dmF'rontlayyoutHelper

--- dmFrontPlugin/lib/view/html/layout/dmFrontLayoutHelper.php
+++ dmFrontPlugin/lib/view/html/layout/dmFrontLayoutHelper.php
@@ -7,7 +7,6 @@ class dmFrontLayoutHelper extends dmCoreLayoutHelper
 
   protected function initialize(array $options)
   {
-    
     // ajout lioshi : erreur 500 déclenchée si le site est inactif
     if (!dmConfig::get('site_active') && !sfContext::getInstance()->getUser()->can('site_view')) {
         throw new dmException('Site is inactive, only user with site_view credentials can see it');
