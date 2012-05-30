4feb73c01001ec297c2228ea36e65bb2a80a3bcd
stephane meron <smeron@lesechos.fr>
2012-05-30 12:29:44 +0200

    Gestion de l'affichage des pages visibles sur la page "plan du site" - Gestion par app

--- dmCorePlugin/plugins/sidSiteMapPlugin/modules/sitemap/templates/_planDuSite.php
+++ dmCorePlugin/plugins/sidSiteMapPlugin/modules/sitemap/templates/_planDuSite.php
@@ -1,5 +1,7 @@
 <?php
 use_stylesheet('/sidSiteMapPlugin/css/sitemap.css');
+
+//$toto = implode("'", sfConfig::get('app_pages-for-sitemap_not-visibles')); echo 'toto :'.$toto.' titi';
 echo _tag('nav', $menu);
 
 
