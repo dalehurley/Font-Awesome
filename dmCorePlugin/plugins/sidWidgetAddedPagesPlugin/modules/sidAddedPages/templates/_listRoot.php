d6b31244454b081c3ff9843037c3fbc56800455c
stephane meron <smeron@lesechos.fr>
2012-06-29 09:53:55 +0200

    Modif pour la présentation du listing

--- dmCorePlugin/plugins/sidWidgetAddedPagesPlugin/modules/sidAddedPages/templates/_listRoot.php
+++ dmCorePlugin/plugins/sidWidgetAddedPagesPlugin/modules/sidAddedPages/templates/_listRoot.php
@@ -1,12 +1,24 @@
 <?php // Vars: $sidAddedPagesPager
-
+$i = 1;
+$i_max = count($sidAddedPagesPager);
+$class = '';
 echo $sidAddedPagesPager->renderNavigationTop();
 
 echo _open('ul.elements');
 
 foreach ($sidAddedPagesPager as $sidAddedPages)
 {
-  echo _open('li.element');
+  // class first ou last pour listing
+            if ($i == 1) {
+                $class = 'first';
+                if ($i == $i_max)
+                    $class = 'first last';
+            }
+            elseif ($i == $i_max)
+                $class = 'last';
+            else
+                $class = '';  
+  echo _open('li.element '.$class);
 
     echo _link($sidAddedPages);
 
