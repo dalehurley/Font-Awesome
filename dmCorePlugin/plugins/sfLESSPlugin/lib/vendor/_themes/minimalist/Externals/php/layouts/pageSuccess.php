1b884251ac3c37fb5163347e17c6f35692e66047
lionel Fenneteau <lionel.fenneteau@gmail.com>
2012-07-10 18:15:16 +0200

    new pageSucces definition and func maj bs Add button GRID display in webdebug bar

--- dmCorePlugin/plugins/sfLESSPlugin/lib/vendor/_themes/minimalist/Externals/php/layouts/pageSuccess.php
+++ dmCorePlugin/plugins/sfLESSPlugin/lib/vendor/_themes/minimalist/Externals/php/layouts/pageSuccess.php
@@ -7,38 +7,12 @@
  * - dmFrontPageHelper $helper      ( page_helper service )
  * - boolean           $isEditMode  ( whether the user is allowed to edit page )
  * 
- * to use grid system of bootstrap
- * <?php echo $helper->renderArea('layout.top', '#dm_header') ?> in that case you must define manually the span and offset of #elements
- * ---------------
- * theme.less
- *  * // layout style
- * .default { // default's layout
- * 	#dm_header 			{ .span(11); .offset(1) }
- * 	#dm_custom_top		{ .span(12) }
- * 	#dm_sidebar_left 	{ .span(2) }
- * 	#dm_page_content 	{ .span(7) }
- * 	#dm_sidebar_right 	{ .span(3) }
- * 	#dm_custom_bottom 	{ .span(12) }
- * 	#dm_footer 			{ .span(12) }
- * }
- * .default-fluid { // default-fluid layout
- * 	#dm_header                    { .span-fluid(12); }
- * 	#dm_custom_top                { .span-fluid(12)  }
- * 	#dm_sidebar_left              { .span-fluid(2)   }
- * 	#dm_page_content              { .span-fluid(7)   }
- * 	#dm_sidebar_right             { .span-fluid(3)   }
- * 	#dm_custom_bottom             { .span-fluid(12)  }
- * 	#dm_footer                    { .span-fluid(12)  }
  * 
- * 	#dm_header:first-child, 
- * 	#dm_custom_top:first-child, 
- * 	#dm_sidebar_left:first-child, 
- * 	#dm_page_content:first-child, 
- * 	#dm_sidebar_right:first-child, 
- * 	#dm_custom_bottom:first-child, 
- * 	#dm_footer:first-child { margin-left: 0; } // 
- * }
- * ---------------
+ * to use grid system of bootstrap
+ * <?php echo $helper->renderArea('layout.top', '#dm_header.span3.offset1') ?> 
+ * in that case you must define manually the spanX and offsetX
+ *
+ * See variables.less to define GRID parameters
  * 
  */
 
@@ -48,21 +22,20 @@
 
 	<div class="dm_layout container">
 		<div class="row">	
-	      		<?php echo $helper->renderArea('layout.top', '#dm_header') ?>
-   		</div>
-		<div class="row">
-			<?php echo $helper->renderArea('layout.customTop', '#dm_custom_top') ?>
+	      		<?php echo $helper->renderArea('layout.top', '#dm_header.span3') ?>
+
+			<?php echo $helper->renderArea('layout.customTop', '#dm_custom_top.span9') ?>
           	</div>
 		<div class="row">
-			<?php echo $helper->renderArea('layout.left','#dm_sidebar_left') ?>
-			<?php echo $helper->renderArea('page.content','#dm_page_content') ?>
-			<?php echo $helper->renderArea('layout.right','#dm_sidebar_right') ?>
+			<?php echo $helper->renderArea('layout.left','#dm_sidebar_left.span4') ?>
+			<?php echo $helper->renderArea('page.content','#dm_page_content.span6') ?>
+			<?php echo $helper->renderArea('layout.right','#dm_sidebar_right.span2') ?>
 		</div>
 		<div class="row">		
-			<?php echo $helper->renderArea('layout.customBottom', '#dm_custom_bottom') ?>      
+			<?php echo $helper->renderArea('layout.customBottom', '#dm_custom_bottom.span12') ?>      
 		</div>
 		<div class="row">
-			<?php echo $helper->renderArea('layout.bottom', '#dm_footer') ?>	
+			<?php echo $helper->renderArea('layout.bottom', '#dm_footer.span12') ?>	
 		</div>
 	</div>
   </div>
