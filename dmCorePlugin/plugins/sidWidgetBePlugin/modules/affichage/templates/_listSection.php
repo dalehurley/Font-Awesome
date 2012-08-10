6d537cb2b814fc86a7ed82112c37f0a78c058e82
stephane meron <smeron@lesechos.fr>
2012-08-10 09:25:31 +0200

    templating pour style v2

--- dmCorePlugin/plugins/sidWidgetBePlugin/modules/affichage/templates/_listSection.php
+++ dmCorePlugin/plugins/sidWidgetBePlugin/modules/affichage/templates/_listSection.php
@@ -7,75 +7,146 @@
 // $nbArticles
 // $nbSections
 // $sectionName
+if(dmConfig::get('site_theme_version') == 'v1'){
+    foreach ($sectionArticles as $section => $sectionArticle) {
 
-foreach ($sectionArticles as $section => $sectionArticle) {
+        if($section) echo '<h5 class="title">'.$section.'</h5>';
 
-    if($section) echo '<h5 class="title">'.$section.'</h5>';
+        //ouverture du listing
+        echo _open('ul.elements');
 
-    //ouverture du listing
-    echo _open('ul.elements');
+        $i = 0;
+        $i_max = count($sectionArticle); // il faut compter le nombre de resultats pour la page en cours, count($articlePager) renvoie la taille complète du pager  
 
-    $i = 0;
-    $i_max = count($sectionArticle); // il faut compter le nombre de resultats pour la page en cours, count($articlePager) renvoie la taille complète du pager  
+        foreach ($sectionArticle as $article) {
+            $i++;
+            $position = '';
+            switch ($i){
+                case '1' : 
+                    if ($i_max == 1) $position = ' first last';
+                    else $position = ' first';
+                    break;
+                default : 
+                    if ($i == $i_max) $position = ' last';
+                    else $position = '';
+                    break;
+            }
 
-    foreach ($sectionArticle as $article) {
-        $i++;
-        $position = '';
-        switch ($i){
-            case '1' : 
-                if ($i_max == 1) $position = ' first last';
-                else $position = ' first';
-                break;
-            default : 
-                if ($i == $i_max) $position = ' last';
-                else $position = '';
-                break;
+
+            //on supprime les photos après les 3 premiers articles
+            $imageLink = '/_images/lea' . $article->filename . '-p.jpg';
+            $imageHtml = '';
+            if (is_file(sfConfig::get('sf_web_dir').$imageLink) && $i < 4 ){  // les 3 premiers articles ont une image
+                $imageHtml =    
+                    '<span class="imageWrapper">'.
+                        '<img src="'.$imageLink.'" itemprop="image" class="image" alt="'.$article->getTitle().'">'.
+                    '</span>';
+            }
+
+            //ajout de l'article
+            echo 
+            '<li itemtype="http://schema.org/Article" itemscope="itemscope" class="element itemscope Article'.$position.'">';
+            echo _link($article)->set('.link.link_box')->text(
+                    $imageHtml.
+                    '<span class="wrapper">'.
+                        '<span class="subWrapper">'.
+                            '<span itemprop="name" class="title itemprop name">'.$article->getTitle().'</span>'.
+                            '<meta content="'.$article->createdAt.'" itemprop="datePublished">'.
+                        '</span>'.
+                        '<span itemprop="description" class="teaser itemprop description">'.$article->getChapeau().'</span>'.
+                    '</span>'
+            );
+            if(count($sectionName) >1){
+            echo _open('span', array('class'=>'navigationWrapper navigationBottom'));
+                echo _open('ul', array('class'=>'elements'));
+                    echo _open('li', array('class'=>'element first last'));
+                    if($this->context->getPage()->getRecord()->getTitle() == 'ec_echeancier'){
+                        echo _link($article->getSection())->text(__('The other dates of ').$sectionName[$article->getSection()->id]);
+                    }
+                    else{                
+                        echo _link($article->getSection())->text(__('The other ').$sectionName[$article->getSection()->id]);
+                    }
+                    echo _close('li');
+                echo _close('ul');
+            echo _close('span');
+            }
+            echo '</li>';
+
         }
 
-
-        //on supprime les photos après les 3 premiers articles
-        $imageLink = '/_images/lea' . $article->filename . '-p.jpg';
-        $imageHtml = '';
-        if (is_file(sfConfig::get('sf_web_dir').$imageLink) && $i < 4 ){  // les 3 premiers articles ont une image
-            $imageHtml =    
-                '<span class="imageWrapper">'.
-                    '<img src="'.$imageLink.'" itemprop="image" class="image" alt="'.$article->getTitle().'">'.
-                '</span>';
-        }
-
-        //ajout de l'article
-        echo 
-        '<li itemtype="http://schema.org/Article" itemscope="itemscope" class="element itemscope Article'.$position.'">';
-        echo _link($article)->set('.link.link_box')->text(
-                $imageHtml.
-                '<span class="wrapper">'.
-                    '<span class="subWrapper">'.
-                        '<span itemprop="name" class="title itemprop name">'.$article->getTitle().'</span>'.
-                        '<meta content="'.$article->createdAt.'" itemprop="datePublished">'.
-                    '</span>'.
-                    '<span itemprop="description" class="teaser itemprop description">'.$article->getChapeau().'</span>'.
-                '</span>'
-        );
-        if(count($sectionName) >1){
-        echo _open('span', array('class'=>'navigationWrapper navigationBottom'));
-            echo _open('ul', array('class'=>'elements'));
-                echo _open('li', array('class'=>'element first last'));
-                if($this->context->getPage()->getRecord()->getTitle() == 'ec_echeancier'){
-                    echo _link($article->getSection())->text(__('The other dates of ').$sectionName[$article->getSection()->id]);
-                }
-                else{                
-                    echo _link($article->getSection())->text(__('The other ').$sectionName[$article->getSection()->id]);
-                }
-                echo _close('li');
-            echo _close('ul');
-        echo _close('span');
-        }
-        echo '</li>';
-
+        //fermeture du listing
+        echo _close('ul.elements');
+        echo '<br/>';
     }
+}
+elseif(dmConfig::get('site_theme_version') == 'v2'){
+    foreach ($sectionArticles as $section => $sectionArticle) {
 
-    //fermeture du listing
-    echo _close('ul.elements');
-    echo '<br/>';
+        if($section) echo '<h5>'.$section.'</h5>';
+
+        //ouverture du listing
+        echo _open('ul.thumbnails');
+
+        $i = 0;
+        $i_max = count($sectionArticle); // il faut compter le nombre de resultats pour la page en cours, count($articlePager) renvoie la taille complète du pager  
+
+        foreach ($sectionArticle as $article) {
+            $i++;
+            $position = '';
+            switch ($i){
+                case '1' : 
+                    if ($i_max == 1) $position = ' first last';
+                    else $position = ' first';
+                    break;
+                default : 
+                    if ($i == $i_max) $position = ' last';
+                    else $position = '';
+                    break;
+            }
+
+
+            //on supprime les photos après les 3 premiers articles
+            $imageLink = '/_images/lea' . $article->filename . '-p.jpg';
+            $imageHtml = '';
+            if (is_file(sfConfig::get('sf_web_dir').$imageLink) && $i < 4 ){  // les 3 premiers articles ont une image
+                $imageHtml =    
+                    '<div class="span">'.
+                        '<img src="'.$imageLink.'" alt="'.$article->getTitle().'">'.
+                    '</div>';
+            }
+
+            //ajout de l'article
+            echo 
+            '<li class="'.$position.'">';
+            echo _link($article)
+            ->set('.thumbnail')
+            ->text(
+                    '<div class="row">'.
+                    $imageHtml.
+                        '<div class="span">'.
+                            '<h5>'.$article->getTitle().'</h5>'.
+                            '<p>'.$article->getChapeau().'</p>'.
+                        '</div>'.
+                    '</div>'
+            );
+            
+            echo '</li>';
+
+        }
+
+        if(count($sectionName) >1){
+            echo _open('li');
+                    if($this->context->getPage()->getRecord()->getTitle() == 'ec_echeancier'){
+                        echo _link($article->getSection())->text(__('The other dates of ').$sectionName[$article->getSection()->id])->set('.btn');
+                    }
+                    else{                
+                        echo _link($article->getSection())->text(__('The other ').$sectionName[$article->getSection()->id])->set('.btn');
+                    }
+            echo _close('li');
+            }
+        //fermeture du listing
+        echo _close('ul');
+        echo '<br/>';
+    }
 }
 
--- dmCorePlugin/plugins/sidWidgetBePlugin/modules/specifiquesBaseEditoriale/templates/_articlesBySectionContextuel.php
+++ dmCorePlugin/plugins/sidWidgetBePlugin/modules/specifiquesBaseEditoriale/templates/_articlesBySectionContextuel.php
@@ -1,6 +1,6 @@
 <?php
 // vars : $articles, $titreBloc, $lien, $length, $rubrique, $section
-
+if(dmConfig::get('site_theme_version') == 'v1'){
 $html = '';
 
 if (count($articles)) { // si nous avons des actu articles
@@ -37,7 +37,10 @@ if (count($articles)) { // si nous avons des actu articles
 	
 	//création d'un tableau de liens à afficher
 	$elements = array();
-	$elements[] = array('title' => ($lien ? $lien : $section->title . ' en ' . $rubrique->name), 'linkUrl' => 'sidActuArticle/list');
+	// avant
+	//$elements[] = array('title' => ($lien ? $lien : $section->title . ' en ' . $rubrique->name), 'linkUrl' => 'sidActuArticle/list');
+	// après
+	$elements[] = array('title' => ($lien ? $lien : $section . ' en ' . $rubrique), 'linkUrl' => $pageLink);
 	
 	$html.= get_partial('global/navigationWrapper', array(
 													'placement' => 'bottom',
@@ -47,3 +50,47 @@ if (count($articles)) { // si nous avons des actu articles
 
 //affichage html en sortie
 echo $html;
+}
+// vars $pageLink
+elseif(dmConfig::get('site_theme_version') == 'v2'){
+	//echo 'fff'.$articles[0]->Section() . ' en ' . $rubrique['name'];
+	if($lien == '')
+		{$lien = $section.
+			' '.
+			__('in').
+			' '.
+			$rubrique;}
+	$i = 0;
+	$i_max = count($articles);
+	if(count($articles) != NULL){
+		echo _tag('h3',$titreBloc);
+		//echo _open('div', array('class' => 'thumbnail'));
+			echo _open('ul', array('class' => 'thumbnails'));
+				foreach($articles as $article){
+					$html = '';
+					$i++;
+			        $position = '';
+			        switch ($i){
+			            case '1' : 
+			                if ($i_max == 1) $position = ' first last';
+			                else $position = ' first';
+			                break;
+			            default : 
+			                if ($i == $i_max) $position = ' last';
+			                else $position = '';
+			                break;
+	        		}
+
+	        		$html .= _open('div', array('class' => 'caption'));
+	        			$html .= _tag('h5', $article->getTitle());
+	        			$html .= _tag('p',stringTools::str_truncate($article->getChapeau(), $length, '(....)', true));
+	        		$html .= _close('div');
+					echo _open('li');
+						echo _link($article)->text($html)->set('.thumbnail '.$position);
+					echo _close('li');
+				}
+			echo _close('ul');
+			echo _link($pageLink)->text($lien)->set('.btn');
+		//echo _close('div');
+	}
+}
