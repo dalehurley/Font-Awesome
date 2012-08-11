a1b02e37ab42076028e7dd6ae44f05d8652f27fd
Lionel Fenneteau <lionel.fenneteau@gmail.com>
2012-08-11 13:32:21 +0200

    fixed: in list admin, no need sort on first col

--- dmAdminPlugin/web/js/dmAdminCtrl.js
+++ dmAdminPlugin/web/js/dmAdminCtrl.js
@@ -45,7 +45,13 @@
       
       this.pagination();
 
-      $("table.tablesorter").tablesorter( { widgets: ['zebra'] } );
+      $("table.tablesorter").tablesorter( { 
+        headers: { 
+            0: { 
+                sorter: false  // first column with check box : no need sort
+            }
+          },
+        widgets: ['zebra'] } );
     },
 
     filters: function()
